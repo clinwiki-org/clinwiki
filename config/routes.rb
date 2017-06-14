c8de40d1aba1a35bb8fe7121d1d97bed597da806
Robert Elwell <robert.elwell@gmail.com>
2017-06-14 16:11:40 -0400

    add support for static routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,36 @@
 Rails.application.routes.draw do
+  scope(path: '/api') do
+    root 'studies#index'
+    resources :annotations
+    resources :reviews
+    resources :tags
 
-  root 'studies#index'
-  resources :annotations
-  resources :reviews
-  resources :tags
+    get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
+    get 'pages/about'
+    get 'pages/contact'
 
-  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
-  get 'pages/about'
-  get 'pages/contact'
+    devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'},
+                       :defaults => { format: 'json'} do
+      get '/users/sign_out' => 'sessions#destroy'
+    end
 
-  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'},
-                     :defaults => { format: 'json'} do
-    get '/users/sign_out' => 'sessions#destroy'
+    get "/user/exists", to: "application#user_exists", defaults: { format: 'json' }
+
+    post "/studies/search/json", to: "studies#search", as: :empty_search_json
+    get "/studies/search/:q", to: "studies#index"
+    post "/studies/search/:q/json", to: "studies#search", as: :studies_search_json
+    get "/studies/:study_id/json", to: "studies#json"
+    get "/studies/:study_id/crowd", to: "studies#crowd"
+    get "/studies/:study_id/administrative", to: "studies#administrative"
+    get "/studies/:study_id/recruitment", to: "studies#recruitment"
+    get "/studies/:study_id/descriptive", to: "studies#descriptive"
+    get "/studies/:study_id/tracking", to: "studies#tracking"
+
+    resources :studies do
+      resources :reviews, except: [:index]
+    end
   end
 
-  get "/user/exists", to: "application#user_exists", defaults: { format: 'json' }
-
-  post "/studies/search/json", to: "studies#search", as: :empty_search_json
-  get "/studies/search/:q", to: "studies#index"
-  post "/studies/search/:q/json", to: "studies#search", as: :studies_search_json
-  get "/studies/:study_id/json", to: "studies#json"
-  get "/studies/:study_id/crowd", to: "studies#crowd"
-  get "/studies/:study_id/administrative", to: "studies#administrative"
-  get "/studies/:study_id/recruitment", to: "studies#recruitment"
-  get "/studies/:study_id/descriptive", to: "studies#descriptive"
-  get "/studies/:study_id/tracking", to: "studies#tracking"
-
-  resources :studies do
-    resources :reviews, except: [:index]
-  end
+  match '*all', to: static("index.html"), via: [:get]
 
 end
