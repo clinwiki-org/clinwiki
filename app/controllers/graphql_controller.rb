e9c0c017107381d5a9593ceb106f98ed934a64d1
Robert Elwell <robert.elwell@gmail.com>
2018-07-22 11:25:38 -0400

    make graphiql endpoint open to the world for now

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,4 +1,7 @@
 class GraphqlController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  skip_before_filter :authenticate_user!
+
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
