af90e1925b814b9605f23bfe0fb9870f0ab037de
Eric Harding <eric.harding@bluetradesys.com>
2018-10-27 22:03:16 -0400

    Include aggs and sorts in AggDropDown query

--- app/gql_containers/SearchPage/components/AggDropDown.tsx
+++ app/gql_containers/SearchPage/components/AggDropDown.tsx
@@ -34,8 +34,8 @@ const DropdownButtonWrapper = styled.div`
 `;
 
 const QUERY_AGG_BUCKETS = gql`
-  query ($agg : String!, $q : String) {
-    aggBuckets(params: {agg: $agg, q: $q}) {
+  query ($agg : String!, $q : String, $sort:[String!], $aggFilters:[AggFilter!]) {
+    aggBuckets(params: {agg: $agg, q: $q, sorts: $sort, aggFilters: $aggFilters }) {
       aggs {
         name
         buckets {
@@ -47,8 +47,8 @@ const QUERY_AGG_BUCKETS = gql`
   }`
 
   const QUERY_CROWD_AGG_BUCKETA = gql`
-  query ($agg : String!, $q: String) {
-    aggBuckets: crowdAggBuckets(params: {agg:$agg, q: $q}) {
+  query ($agg : String!, $q: String, $sort:[String!], $aggFilters:[AggFilter!]) {
+    aggBuckets: crowdAggBuckets(params: {agg:$agg, q: $q, sorts: $sort, aggFilters: $aggFilters }) {
       aggs {
         buckets {
           key
