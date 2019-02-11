1aca3e0268c0864a017fa070c53bb54f9bb30fc7
Eric Harding <eric.harding@bluetradesys.com>
2019-02-11 08:40:26 -0500

    Try disable bootsnap

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+# require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
 if ENV['DOCKER']
   require 'rails/commands/server'
