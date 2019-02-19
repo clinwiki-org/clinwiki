af25dad27b78e6de38b61d5cbac04be0524f65b2
Eric Harding <eric.harding@bluetradesys.com>
2019-02-19 10:31:30 -0500

    updated intervention by id

--- app/graphql/types/intervention_type.rb
+++ app/graphql/types/intervention_type.rb
@@ -6,13 +6,13 @@ module Types
     field :type, String, "Intervention type", null: true, method: :intervention_type
     field :name, String, "Intervention name", null: true
     field :description, String, "Intervention description", null: true
-    field :wikipedia_url,
-          String,
-          "Link to wikipedia article derived from name. Use sparingly as it actually hits wikipedia api to get the url",
+    field :wikipedia_article,
+          WikipediaArticleType,
+          "Wikipedia article searched by name. Use sparingly as it actually hits wikipedia api to get the data.",
           null: true
 
-    def wikipedia_url
-      Wikipedia.url_for(search_text: object.name)
+    def wikipedia_article
+      Wikipedia.find_article(search_text: object.name)
     end
   end
 end
