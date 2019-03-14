86c1fd26e1f6d2c3071099c98a8351a770e9073f
Eric Harding <eric.harding@bluetradesys.com>
2019-03-14 19:36:38 -0400

    bug: fix workflow sorted by start/end date

--- app/services/study_edge_service.rb
+++ app/services/study_edge_service.rb
@@ -1,3 +1,6 @@
+LLONG_MIN = -9223372036854775808 # rubocop:disable Style/NumericLiterals
+LLONG_MAX = 9223372036854775807 # rubocop:disable Style/NumericLiterals
+
 class StudyEdgeService
   # @param params - hash representing SearchInputType with symbols as keys.
   def initialize(params)
@@ -13,7 +16,7 @@ class StudyEdgeService
 
     OpenStruct.new(
       next_id: next_study_id(study: study),
-      prev_id: next_study_id(study: study, forward: false),
+      prev_id: next_study_id(study: study, reverse: true),
       is_workflow: is_workflow,
       study: study,
     )
@@ -25,26 +28,73 @@ class StudyEdgeService
     @search_service.search&.dig(:studies)&.first&.id
   end
 
-  def next_study_id(study:, forward: true)
+  # There's a big problem with nulls. When you sort by a field
+  # (in this case we considered only dates) with null values,
+  # nulls will always come last, regardless of whether you do asc or desc search
+  # This is achieved by elastic with assigning LLONG_MIN value to date if we're searching
+  # desc and LLONG_MAX if we're searcing asc. There are 4 cases:
+  # 1) asc sort + current field value is not nil - this is trivial,
+  # just use the current value in the sort params
+  # 2) asc sort + current field value is nil - then use LLONG_MAX as current study sort value
+  # 3) desc sort + current field value is nil. In this case we would like to have a record with
+  # nil value or if it doesn't exist the first non-nil record. This is basically done as setting
+  # sort value to LLONG_MIN - then elastic will return all the nil values. If that result is missing
+  # then by setting sort value to LLONG_MAX we get the first non-nil value.
+  # When there are potentially several columns to sort by with nils we take 2 variants for each nil entry.
+  # 4) desc sort + current field value is not nil. This is a TODO. Currently the result (prev study)
+  # is returned even for the 1st entry. That could be fixed, but needs more conding time.
+  def next_study_id(study:, reverse: false)
     return nil if study.blank?
 
-    @search_service.search(
-      search_after: sort_params_values(study),
-      reverse: !forward,
-    )&.dig(:studies)&.first&.id
+    sort_values_variants(study, reverse).each do |sort_values|
+      id = @search_service.search(
+        search_after: sort_values,
+        reverse: reverse,
+      )&.dig(:studies)&.first&.id
+      return id unless id.nil?
+    end
+
+    nil
   end
 
-  def sort_params_values(study)
+  # All variants of sort_params as specified in clause 3 above.
+  def sort_values_variants(study, reverse)
+    sort_params = sort_values(study, reverse)
+    res = [sort_params]
+    sort_params.reverse.each_with_index do |sort_value, i|
+      next if sort_value != LLONG_MIN
+
+      params = sort_params.deep_dup
+      params[sort_params.size - i - 1] = LLONG_MAX
+      res << params
+    end
+    res
+  end
+
+  def sort_values(study, reverse)
     return nil if study.blank?
 
     sorts = @params[:sorts] || []
-    sorts.push(id: :nct_id) unless sorts.any? { |x| x[:id].to_sym == :nct_id }
+    sorts.push(id: "nct_id", desc: false) unless sorts.any? { |x| x[:id].to_sym == :nct_id }
     sorts.map do |hash|
       name = hash[:id]
       value = study.send(name)
-      # there is a bug in Searchkick - when you pass 0.0 it converts it to "0.0"
-      # and Elasticsearch fails to parse that
-      value.is_a?(BigDecimal) ? value.to_i : value
+      sort_value_to_elastic(value, hash[:desc] ^ reverse)
+    end
+  end
+
+  def sort_value_to_elastic(value, desc)
+    case value
+    # there is a bug in Searchkick - when you pass 0.0 it converts it to "0.0"
+    # and Elasticsearch fails to parse that
+    when BigDecimal
+      value.to_i
+    when Date
+      value.strftime("%Q")
+    when nil
+      desc ? LLONG_MIN : LLONG_MAX
+    else
+      value
     end
   end
 end
