8eb55060167bc8a64c6b7cf2d8c67c5fd4b5a10e
Eric Harding <eric.harding@bluetradesys.com>
2019-04-17 19:48:11 -0400

    Second attempt keep dev in line

--- app/workers/aact_sync.rb
+++ app/workers/aact_sync.rb
@@ -2,6 +2,9 @@ class AactSync
   include Sidekiq::Worker
 
   def perform(opts = {})
-    Study.enqueue_reindex_job_big(Study.where("updated_at > ?", opts.fetch("days_ago", 1).to_i.days.ago))
+    days_ago = opts.fetch("days_ago", 1).to_i
+    return if days_ago == 0
+
+    Study.enqueue_reindex_job_big(Study.where("updated_at > ?", days_ago.days.ago))
   end
 end
