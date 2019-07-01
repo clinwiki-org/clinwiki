b63fd2460450cc2e236ff3fad366c7e5121a1c74
Nicholas Tran <ntran6997@berkeley.edu>
2019-07-01 14:05:00 -0700

    merged my autocomplete changes with master

---
+++ app/containers/SearchPage/components/FCTest.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {FormControl} from 'react-bootstrap';
+
+class FCTest extends FormControl {
+	constructor(props){
+    	super(props);
+  }
+
+} 
+
+export default FCTest;
