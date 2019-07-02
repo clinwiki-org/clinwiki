10103cb83c6a0517f0fe1e2035d8334727e6c8b7
armantark <armantarkhanian@berkeley.edu>
2019-07-01 18:20:22 -0700

    new word frequencies table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_135833) do
+ActiveRecord::Schema.define(version: 2019_07_02_010935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,14 @@ ActiveRecord::Schema.define(version: 2019_05_04_135833) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["nct_id"], name: "index_wiki_pages_on_nct_id", unique: true
+  end
+
+  create_table "word_frequencies", force: :cascade do |t|
+    t.string "name"
+    t.integer "rank"
+    t.float "frequency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "site_views", "sites"
