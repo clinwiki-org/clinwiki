b82d05dd4c59f5018cee62998448acb7adc8fe11
armantark <armantarkhanian@berkeley.edu>
2019-07-02 15:45:37 -0700

    added some test entries to the WordFreq table in seeds.rb. make sure to run rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@ User.create! first_name: "Sheri", last_name: "Tibbs", email: "sheri.tibbs@gmail.
              password: "clinwiki", password_confirmation: "clinwiki"
 User.create! first_name: "William", last_name: "Hoos", email: "william.hoos@gmail.com",
              password: "clinwiki", password_confirmation: "clinwiki"
+
+WordFrequency.create! name: "cancer", frequency: 10, rank: 1
+WordFrequency.create! name: "test", frequency: 5, rank: 2
+WordFrequency.create! name: "kidney", frequency: 3, rank: 3
+WordFrequency.create! name: "lung", frequency: 2, rank: 4
+WordFrequency.create! name: "prostate", frequency: 1, rank: 5
