865a6b0fac32fa676d3e16b7d688248f7e5d6455
armantark <armantarkhanian@berkeley.edu>
2019-07-01 22:26:04 -0700

    added primary key to be name inside of the word freq table

--- app/models/word_frequency.rb
+++ app/models/word_frequency.rb
@@ -1,2 +1,11 @@
 class WordFrequency < ApplicationRecord
+  self.primary_key = "name"
+
+  def totalFrequency
+    WordFrequency.sum(:frequency)
+  end
+
+  def percentage(word)
+    WordFrequency.where(:name => word).frequency / totalFrequency
+  end
 end
