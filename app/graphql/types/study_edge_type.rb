855a601c0e5a2031f0360d3a0e2e66226154e744
armantark <armantarkhanian@berkeley.edu>
2019-07-30 17:18:04 -0700

    loose ends

--- app/graphql/types/study_edge_type.rb
+++ app/graphql/types/study_edge_type.rb
@@ -7,8 +7,8 @@ module Types
     field :isWorkflow, Boolean, "Study is in a workflow mode", null: false
     field :workflowName, String, "Workflow name if any", null: true
     field :study, StudyType, "Study", null: false
-    field :counterIndex, Integer, "The index of the study in the results", null: true
     field :recordsTotal, Integer, "Total number of records", null: false
+    field :counterIndex, Integer, "The index of the study in the results", null: true
     field :firstId, String, "Id of the first study", null: true
     field :lastId, String, "Id of the last study", null: true
   end
