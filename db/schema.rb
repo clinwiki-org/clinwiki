4a1be2e7520740968ffb3dac5cf517b9354615d6
nicktran6997 <nicktran6997@berkeley.edu>
2019-08-05 16:58:48 +0000

    minor fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_07_18_071212) do
 
   # These are extensions that must be enabled in order to support this database
@@ -144,7 +143,6 @@ ActiveRecord::Schema.define(version: 2019_07_18_071212) do
 
   create_table "word_frequencies", force: :cascade do |t|
     t.string "name"
-    t.integer "rank"
     t.integer "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
