d646db06c860d7e384d9b15c8a2fad7abf2b2141
Eric Harding <eric.harding@bluetradesys.com>
2019-09-14 21:31:08 -0400

    Update generated types

---
+++ front/app/types/SuggestionsQuery.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: SuggestionsQuery
+// ====================================================
+
+export interface SuggestionsQuery {
+  /**
+   * searchkick recommended words
+   */
+  typeahead: string[];
+}
+
+export interface SuggestionsQueryVariables {
+  params?: string | null;
+}
