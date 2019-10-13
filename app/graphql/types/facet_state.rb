cb3c08f91abdbb46d9a8662c135b44aed2c1a637
Eric Harding <eric.harding@bluetradesys.com>
2019-10-12 21:08:15 -0400

    Missing field in facet_state

--- app/graphql/types/facet_state.rb
+++ app/graphql/types/facet_state.rb
@@ -2,6 +2,7 @@ module Types
   class FacetState < BaseInputObject
     description "Describe the state of a single facet."
     argument :name, String, "facet name", required: true
+    argument :value, String, "facet value", required: true
     argument :enable, Boolean, "", required: true
   end
 end
