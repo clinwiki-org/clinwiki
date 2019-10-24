13ac57c819b5d81c0d4478b661155d326ee64b9e
Eric Harding <eric.harding@bluetradesys.com>
2019-10-23 21:38:13 -0400

    Remove note about video on landing page from staging

--- front/app/containers/ReleaseNotes/version.tsx
+++ front/app/containers/ReleaseNotes/version.tsx
@@ -11,7 +11,6 @@ const notes =
 
 ### Version 2
 - Add /voyager to debug builds for exploring graphql schema
-- Add youtube video to home page
 - Add link to NCT ID to address issue #162 - thanks rarbuthnot
 
 ### Version 1
