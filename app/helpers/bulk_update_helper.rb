749e84bb8e212f65f3bbd92ed3d9a02ee75d3221
Eric Harding <eric.harding@bluetradesys.com>
2019-10-31 11:00:26 -0400

    bulk update fixes

--- app/helpers/bulk_update_helper.rb
+++ app/helpers/bulk_update_helper.rb
@@ -1,40 +1,23 @@
 module BulkUpdateHelper
-  # bulk_params: [{
-  #   study: Study,
-  #   add_meta: {
-  #     "color": ["red", "blue"]
-  #   }
-  #   delete_meta: {
-  #     "color": ["green"]
-  #   }
-  # }]
   def bulk_create_or_update_wiki_page_for_study(bulk_params: [], user: nil)
     ActiveRecord::Base.logger = nil
-    # start = Time.now
     edits = []
     wiki_pages = []
     bulk_params.each do |params|
-      # start2 = Time.now
       study = params[:study]
       wiki_page = study.wiki_page || WikiPage.new(nct_id: study.nct_id)
       wiki_text = wiki_page.text
-      # pp "TIME INITIALIZING #{Time.now - start2}"
       wiki_page.front_matter = update_front_matter(wiki_page.front_matter, params)
       wiki_page.updater = user
-
       next unless wiki_page.text != wiki_text
 
-      # start3 = Time.now
       wiki_pages << wiki_page
-      edits << generate_edit(wiki_page, wiki_text, user)
-      # pp "TIME PUSHING #{Time.now - start3}"
-      # finished = Time.now
-      # pp "TIME #{study[:nct_id]} - #{finished - start2}"
+      edits << { wiki_page: wiki_page, wiki_text: wiki_text, user: user }
     end
 
     ActiveRecord::Base.transaction do
       WikiPage.import wiki_pages, on_duplicate_key_update: { conflict_target: [:nct_id], columns: [:text] }
-      WikiPageEdit.import edits
+      GenerateEditsJob.perform_later edits
     end
     Study.enqueue_reindex_ids(bulk_params.map { |x| x[:study][:nct_id] })
   end
@@ -53,28 +36,14 @@ module BulkUpdateHelper
       params[:delete_meta].each do |key, remove_values|
         values = updated[key]&.split("|") || []
         values = values.reject { |x| remove_values.include?(x) }
-        updated[key] = values.join("|")
+        if values.empty?
+          updated.delete key
+        else
+          updated[key] = values.join "|"
+        end
       end
     end
     updated
-  end
-
-  def generate_edit(wiki_page, wiki_text, user)
-    # start = Time.now
-    diff = Diffy::Diff.new(wiki_page.text, wiki_text)
-    diff_s = diff.to_s
-    diff_html = diff.to_s(:html_simple)
-    # pp "generate_edit DIFF #{Time.now-start}"
-    # start2 = Time.now
-    wp = {
-      user_id: user.id,
-      wiki_page_id: wiki_page.id,
-      diff: diff_s,
-      diff_html: diff_html,
-    }
-    # pp "generate_edit NEW #{Time.now-start2}"
-    # pp "generate_edit TOTAL #{Time.now-start}"
-    wp
   end
 
   def build_bulk_params(studies, agg_state)
@@ -118,6 +87,4 @@ module BulkUpdateHelper
       }
     end
   end
-
-
 end
