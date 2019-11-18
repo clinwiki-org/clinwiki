d528fe07ce31bd9bbabf35a73f3bec64de5c249b
Eric Harding <eric.harding@bluetradesys.com>
2019-11-18 11:59:51 -0500

    Address index refresh delay #178

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,5 +2,6 @@
 schedule_file = "config/schedule.yml"
 
 if File.exists?(schedule_file) && Sidekiq.server?
+  Sidekiq.options[:poll_interval] = 5;
   Sidekiq::Cron::Job.load_from_hash YAML.load(ERB.new(File.read(schedule_file)).result)
 end
