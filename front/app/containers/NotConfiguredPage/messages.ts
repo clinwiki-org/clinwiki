4dbd55618b3ea49cfdf6bdd7bbed6c1376851876
Eric Harding <eric.harding@bluetradesys.com>
2019-12-10 11:36:08 -0500

    #177 mistyped subdomains

---
+++ front/app/containers/NotConfiguredPage/messages.ts
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.NotFoundPage.header',
+    message:" TEST",
+    defaultMessage: 'The subdomain "{subdomain}" is not configured!',
+  },
+});
