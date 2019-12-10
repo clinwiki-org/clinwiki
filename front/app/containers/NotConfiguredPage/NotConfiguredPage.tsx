92d9e4f2a0b0f54b3feb206f1bfdccd18d831ec1
Eric Harding <eric.harding@bluetradesys.com>
2019-12-10 11:36:08 -0500

    #177 mistyped subdomains

---
+++ front/app/containers/NotConfiguredPage/NotConfiguredPage.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
+import {RouteComponentProps} from 'react-router-dom';
+import * as queryString from 'query-string';
+import messages from './messages';
+
+export default class NotConfiguredPage extends React.PureComponent<RouteComponentProps> {
+  render() {
+    const { subdomain } = queryString.parse(this.props.location.search);
+    return (
+      <h1>
+        <FormattedMessage {...messages.header}  values={{ subdomain }}/>
+      </h1>
+    );
+  }
+}
