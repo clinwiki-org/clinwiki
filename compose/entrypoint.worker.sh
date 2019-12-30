c0fce9b785c28d48ebf9740545a7ad282c9c172c
Eric Harding <eric.harding@bluetradesys.com>
2019-12-30 08:58:02 -0500

    Add CW_MODE argument to docker-compose config [PROD, WORKER, FAST, DEV]

--- compose/entrypoint.worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bundle install
-bundle exec rake db:create
-bundle exec rake db:migrate
-
-# This line executes the CMD from Dockerfile, command from docker-compose file or if you docker run
-exec "$@"
