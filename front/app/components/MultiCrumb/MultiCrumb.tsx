e3bae1620c6b458e0cbbcc42a751e7e106aeb5eb
Brian Espinosa <bespinosa1931@gmail.com>
2020-01-28 12:19:57 -0600

    Adds a toggle for facets that have more than 4 values selected

--- front/app/components/MultiCrumb/MultiCrumb.tsx
+++ front/app/components/MultiCrumb/MultiCrumb.tsx
@@ -1,18 +1,47 @@
 import * as React from 'react';
 import { Label } from 'react-bootstrap';
 import * as FontAwesome from 'react-fontawesome';
-
-const MultiCrumb = (props: {
+import { render } from 'react-dom';
+interface MultiCrumbProps{
   category?: string;
   values: string[];
   labels?: string[];
   onClick: (s: string) => void;
-}) => {
-  return (
+}
+interface MultiCrumbState{
+  showValue: boolean;
+}
+class MultiCrumb extends React.Component<MultiCrumbProps, MultiCrumbState>{
+
+state: MultiCrumbState={
+  showValue: false
+};
+
+// (props: {
+//   category?: string;
+//   values: string[];
+//   labels?: string[];
+//   onClick: (s: string) => void;
+// }) => {
+toggleShowValue=()=>{
+  const {showValue} = this.state
+
+  this.setState({showValue: !showValue})
+}
+render(){  return (
     <Label className="btn">
-      {props.category && <i>{props.category}:</i>}
-      {props.values.map((v, i) => {
-        const label = props.labels ? props.labels[i] : v;
+      {this.props.category && <i>{this.props.category}:</i>}
+
+      { this.props.values.length > 4 ?(
+
+  <b  onClick={()=>this.toggleShowValue()}>
+   ({this.props.values.length})
+  </b>
+
+      ):(
+
+      this.props.values.map((v, i) => {
+        const label = this.props.labels ? this.props.labels[i] : v;
         return (
           <b key={v}>
             {label}
@@ -24,13 +53,34 @@ const MultiCrumb = (props: {
                 color: '#cc1111',
                 margin: '0 0 0 3px',
               }}
-              onClick={() => props.onClick(v)}
+              onClick={() => this.props.onClick(v)}
             />
           </b>
         );
-      })}
-    </Label>
-  );
+      }))}
+  
+{this.state.showValue==true ?
+(  this.props.values.map((v, i) => {
+        const label = this.props.labels ? this.props.labels[i] : v;
+        return (
+          <b key={v}>
+            {label}
+            <FontAwesome
+              className="remove"
+              name="remove"
+              style={{
+                cursor: 'pointer',
+                color: '#cc1111',
+                margin: '0 0 0 3px',
+              }}
+              onClick={() => this.props.onClick(v)}
+            />
+          </b>
+        );
+      })):(null)}
+
+  </Label>
+  )}
 };
 
 export default MultiCrumb;
