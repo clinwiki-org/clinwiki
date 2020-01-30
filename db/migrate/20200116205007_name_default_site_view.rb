ebcba1c934b84c88c5f5da2a25158f28e2143b74
Ramiro Rodriguez <ramiro.rodriguez@gmail.com>
2020-01-30 15:08:18 -0500

    fixed migration bug

--- db/migrate/20200116205007_name_default_site_view.rb
+++ db/migrate/20200116205007_name_default_site_view.rb
@@ -4,6 +4,6 @@ class NameDefaultSiteView < ActiveRecord::Migration[5.2]
     add_column :site_views, :default, :boolean, default: false, null:false
     add_column :site_views, :description, :string, default: ""
     add_column :site_views, :url, :string, default: ""
-    SiteViews.update_all(default:true)
+    SiteView.update_all(default:true)
   end
 end
