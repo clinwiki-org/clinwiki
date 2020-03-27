aae7e167cce6345176c40163ed5e31719647fd8d
Brian Espinosa <bespinosa1931@gmail.com>
2020-03-27 16:04:29 -0500

    lowercase logic for url final take

--- front/app/containers/SearchPage/SearchPage.tsx
+++ front/app/containers/SearchPage/SearchPage.tsx
@@ -756,8 +756,11 @@ class SearchPage extends React.Component<SearchPageProps, SearchPageState> {
           const siteViewUrl = this.props.match.params.siteviewUrl;
           const siteViews = site.siteViews;
           let currentSiteView =
-            siteViews.find(siteview => siteview.url == siteViewUrl) ||
-            site.siteView;
+          siteViews.find(
+            siteview =>
+              //@ts-ignore
+              siteview.url.toLowerCase() == siteViewUrl.toLowerCase()
+          ) || site.siteView;
 
           return (
             <CrumbsBar
