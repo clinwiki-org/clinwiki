59864193b4ff77a16a8c8542ea057ae10cda573c
Daniel Blat <blatde91@gmail.com>
2020-03-27 15:55:58 -0400

    url string works

--- front/app/components/SiteItem/SiteViewItem.tsx
+++ front/app/components/SiteItem/SiteViewItem.tsx
@@ -99,9 +99,10 @@ class SiteViewItem extends React.PureComponent<SiteViewItemProps> {
   };
 
   render() {
-    const { siteView } = this.props;
-    const urlString = 'clinwiki.org/search/' + siteView.url;
-    // console.log(urlString);
+    const { siteView, site } = this.props;
+    console.log(this.props.site);
+    const urlString = `https://${site.subdomain}.clinwiki.org/search/${siteView.url}`;
+
     return (
       <tr>
         <td>{siteView.name}</td>
@@ -118,7 +119,9 @@ class SiteViewItem extends React.PureComponent<SiteViewItemProps> {
           </UpdateSiteViewMutation>
         </td>
         <td>
-          <Link to={`/search/${siteView.url}`}>{urlString}</Link>
+          <a target="_blank" href={urlString}>
+            {urlString}
+          </a>
         </td>
         <td>
           <StyledButton onClick={this.handleEditClick}>Edit</StyledButton>
