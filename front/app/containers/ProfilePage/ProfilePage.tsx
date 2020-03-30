ec83087a44c647a96260120b6c46f317ccd85733
Brian Espinosa <bespinosa1931@gmail.com>
2020-03-30 16:51:56 -0500

    created route 'profile/:id/' and corresponding container

---
+++ front/app/containers/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import styled from "styled-components";
+import SearchPage from 'containers/SearchPage';
+import { match } from 'react-router-dom';
+
+interface ProfilePageProps {
+  history: History;
+}
+
+
+
+class SitesPage extends React.PureComponent<ProfilePageProps> {
+
+  componentDidMount(){
+    console.log("This.props", this.props)
+  }
+
+  render() {
+    return (
+      <div>
+     <h2>Profile Page</h2>
+      {/* <SearchPage/> */}
+      </div>
+    );
+  }
+}
+
+export default SitesPage;
