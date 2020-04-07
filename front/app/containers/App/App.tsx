982036db16227e7a69321b8f2bbd47321af10c41
Brian Espinosa <bespinosa1931@gmail.com>
2020-04-07 15:19:53 -0500

    some cleanup

--- front/app/containers/App/App.tsx
+++ front/app/containers/App/App.tsx
@@ -49,15 +49,10 @@ class App extends React.PureComponent<AppProps> {
             <Route exact path="/" component={LandingPage} />
             <Route exact path="/about" component={AboutPage} />
             <Route exact path="/version" component={ReleaseNotes} />
-            {/* <Redirect exact from="/search/" to="/search/default" /> */}
             <Route
               path="/search/"
               component={SearchPage}
             />
-            {/* <Route
-              path="/search/:siteviewUrl/:searchId"
-              component={SearchPage}
-            /> */}
             <Route path="/search/:siteviewUrl" component={SearchPage} />
             <Route
               path="/study/:nctId/review/:reviewId/edit"
