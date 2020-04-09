d2c8bbb5e65cd07938c36775264b1c9b6d211089
Daniel Blat <blatde91@gmail.com>
2020-04-09 16:59:14 -0400

    removed hash=

--- front/app/components/AuthHeader/AuthHeader.tsx
+++ front/app/components/AuthHeader/AuthHeader.tsx
@@ -55,7 +55,7 @@ export class AuthHeader extends React.PureComponent<AuthHeaderProps> {
           style={{ paddingLeft: '15px', paddingRight: '15px' }}>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link id="logo" to="/search?hash=&sv=default">
+              <Link id="logo" to="/search?sv=default">
                 ClinWiki <span id="small">(beta)</span>
               </Link>
             </Navbar.Brand>
@@ -64,7 +64,7 @@ export class AuthHeader extends React.PureComponent<AuthHeaderProps> {
           <Navbar.Collapse>
             <Nav pullRight>
               {true ? null : (
-                <NavItem eventKey={1} href="/search?hash=&sv=default">
+                <NavItem eventKey={1} href="/search?sv=default">
                   Search
                 </NavItem>
               )}
