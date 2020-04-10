ca0f926f0308946eaacfaa0c08c49d2f5747e219
williamhoos <williamhoos@gmail.com>
2020-04-10 16:11:06 -0400

    fix donation link
    remove target_blank and change eventKey to 2

--- front/app/components/AuthHeader/AuthHeader.tsx
+++ front/app/components/AuthHeader/AuthHeader.tsx
@@ -68,7 +68,7 @@ export class AuthHeader extends React.PureComponent<AuthHeaderProps> {
                   Search
                 </NavItem>
               )}
-              <NavItem eventKey={1} href="https://home.clinwiki.org/make-a-donation/" target=_blank>
+              <NavItem eventKey={2} href="https://home.clinwiki.org/make-a-donation/">
                 Donate
               </NavItem>              
               <NavItem eventKey={1} href="https://home.clinwiki.org/">
