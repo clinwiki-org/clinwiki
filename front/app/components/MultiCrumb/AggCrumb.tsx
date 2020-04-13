75f89d6875b390ff202669c8f6a19df5584d667e
Brian Espinosa <bespinosa1931@gmail.com>
2020-04-13 16:30:13 -0500

    conditionals added in getMinString() and getMaxString()

--- front/app/components/MultiCrumb/AggCrumb.tsx
+++ front/app/components/MultiCrumb/AggCrumb.tsx
@@ -7,19 +7,21 @@ import { withSearchParams } from 'containers/SearchPage/components/SearchParamsC
 import ValuesCrumb from './ValuesCrumb';
 import ValueCrumb from './ValueCrumb';
 import CrumbWrapper from './CrumbWrapper';
+import { SiteFragment } from 'types/SiteFragment';
 
 interface AggCrumbProps {
   grouping: string;
   agg: AggFilterListItem;
   searchParams: SearchParams | any;
   updateSearchParams: any;
+  thisSiteView: SiteFragment;
 }
 
 interface AggCrumbState {}
 
 class AggCrumb extends React.Component<AggCrumbProps, AggCrumbState> {
   render() {
-    const { agg, grouping, searchParams, updateSearchParams } = this.props;
+    const { agg, grouping, searchParams, updateSearchParams, thisSiteView } = this.props;
     const updater = new AggFilterInputUpdater(
       agg.field,
       searchParams,
@@ -36,12 +38,12 @@ class AggCrumb extends React.Component<AggCrumbProps, AggCrumbState> {
         />
       );
     } else if (agg.lte || agg.gte) {
-      let label = `${updater.getMinString()} — ${updater.getMaxString()}`;
+      let label = `${updater.getMinString(thisSiteView)} — ${updater.getMaxString(thisSiteView)}`;
       if (!agg.lte) {
-        label = `≥ ${updater.getMinString()}`;
+        label = `≥ ${updater.getMinString(thisSiteView)}`;
       }
       if (!agg.gte) {
-        label = `≤ ${updater.getMaxString()}`;
+        label = `≤ ${updater.getMaxString(thisSiteView)}`;
       }
       crumb = (
         <ValueCrumb label={label} onClick={() => updater.removeRange()} />
--- front/app/containers/SearchPage/components/AggFilterInputUpdater.tsx
+++ front/app/containers/SearchPage/components/AggFilterInputUpdater.tsx
@@ -134,35 +134,41 @@ abstract class AbstractAggFilterInputUpdater {
     return this.agg.match(/date/) !== null;
   }
 
-  getMinString(): string | undefined {
+  getMinString(thisSiteView): string | undefined {
     // need to check for agg type once we start using this for more than date.
     if (this.input.gte) {
-      let type = 'number_range';
-      if (type == 'date_range') {
+      const thisField:any = find(propEq('name', this.agg))(
+        thisSiteView.search.aggs.fields
+      );
+      if (thisField && thisField.display == 'DATE_RANGE') {
         return this.isDateAgg()
           ? moment(this.input.gte)
               .utc(false)
               .format('YYYY-MM-DD')
           : this.input.gte;
-      } else if (type == 'number_range') {
+      } else if (thisField && thisField.display == 'NUMBER_RANGE') {
         return this.input.gte;
       }
+      return this.input.gte;
     }
   }
 
-  getMaxString(): string | undefined {
+  getMaxString(thisSiteView): string | undefined {
     // need to check for agg type once we start using this for more than date.
     if (this.input.lte) {
-      let type = 'number_range';
-      if (type == 'date_range') {
+      const thisField:any = find(propEq('name', this.agg))(
+        thisSiteView.search.aggs.fields
+      );
+      if (thisField && thisField.display == 'DATE_RANGE') {
         return this.isDateAgg()
           ? moment(this.input.lte)
               .utc(false)
               .format('YYYY-MM-DD')
           : this.input.lte;
-      } else if (type == 'number_range') {
+      } else if (thisField && thisField.display == 'NUMBER_RANGE') {
         return this.input.lte;
       }
+      return this.input.lte;
     }
   }
 
