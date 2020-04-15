87d0a9f0c924d7548e2bfc05c0dcc26ded6266c1
hifilorau <tomrau@gmail.com>
2020-04-15 13:30:13 -0400

    added SiteProvider to withTheme so eventually can pull theme values from db

--- front/app/containers/ThemeProvider/ThemeProvider.tsx
+++ front/app/containers/ThemeProvider/ThemeProvider.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import SiteProvider from 'containers/SiteProvider';
 
 interface ThemeProviderProps {
   // id?: number;
@@ -6,52 +7,31 @@ interface ThemeProviderProps {
   // children: (site: SiteFragment, refetch: any) => React.ReactNode;
 }
 
-//this obj is more for reference than anything else
-const clinwikiColors = {
-  //header font color
-  whiteHeaderFont: '#fff',
-  grayHeaderFont: '#777777',
-  //darkBlue for header
-  navBar: '#1b2a38',
-  //button Green
-  button: '#55B88D',
-  //hover button
-  buttonHover: '#e6e6e6',
-  //hover buttonborder
-  buttonBorderHover: '#adadad',
-  //agg side bar gray
-  sideBarBackground: '#4d5863',
-};
 
-export const withTheme = Component => {
-  class ThemeProvider extends React.Component {
-    theme = () => {
-      let primaryColor = 'white';
-      let secondaryColor = 'purple';
-      return {
-        primaryColor: 'white',
-        secondaryColor: 'purple',
-        authHeader: {
-          headerBackground: '#1b2a38',
-          font: '#777777',
-          hoverFont: '#fff',
-          logoFont: '#fff',
-        },
-        authButton: {
-          button: '#55B88D',
-          buttonFont: '#fff',
-          buttonHover: '#e6e6e6',
-          buttonBorderHover: '#adadad',
-        },
-        aggSideBar: {
-          sideBarBackground: '#4d5863',
-        },
-      };
-    };
 
-    render() {
-      return <Component theme={this.theme()} {...this.props} />;
-    }
+export const withTheme = (Component)  =>  {
+ class ThemeProvider extends React.Component {
+
+   theme = (site) => {
+     console.log(site);
+     return {
+       primaryColor: 'white',
+       secondaryColor: 'purple'
+     }
+   }
+
+   render() {
+     return (
+       <SiteProvider>
+        {(site) => 
+        <Component
+          theme={this.theme(site)}
+        {...this.props}
+       />
+        }
+       </SiteProvider>
+     )
+   }
   }
   return ThemeProvider;
 };
