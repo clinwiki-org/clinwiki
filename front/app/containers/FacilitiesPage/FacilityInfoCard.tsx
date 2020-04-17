dbb1e340c5d8cbe6ccd36d38cea0d1a4b89ece5c
Daniel Blat <blatde91@gmail.com>
2020-04-17 16:40:15 -0400

    map cards styles

--- front/app/containers/FacilitiesPage/FacilityInfoCard.tsx
+++ front/app/containers/FacilitiesPage/FacilityInfoCard.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import withTheme from 'containers/ThemeProvider';
 
 const CardContainer = styled.div`
   min-width: 250px;
-  background-color: #55b88d;
+  background-color: ${props => props.theme.mapSection.facilityCardColor};
   min-height: 75px;
   position: relative;
   bottom: 85px;
@@ -15,6 +16,8 @@ const CardContainer = styled.div`
   justify-content: space-between;
   cursor: text;
 `;
+
+const ThemedCardContainer = withTheme(CardContainer);
 
 const TitleText = styled.h1`
   color: white;
@@ -33,11 +36,13 @@ const Pointer = styled.div`
   height: 0;
   border-left: 10px solid transparent;
   border-right: 10px solid transparent;
-  border-top: 10px solid #55b88d;
+  border-top: 10px solid ${props => props.theme.mapSection.facilityCardColor};
   position: relative;
   bottom: 85px;
   right: -5px;
 `;
+
+const ThemedPointer = withTheme(Pointer);
 
 const ContactInfo = styled.p`
   color: white;
@@ -62,12 +67,12 @@ class FacilityInfoCard extends React.PureComponent<any> {
     const { name, address, hover } = this.props;
     return (
       <div>
-        <CardContainer
+        <ThemedCardContainer
           style={hover ? { visibility: 'visible' } : { visibility: 'hidden' }}>
           <TitleText>{this.truncateString(name, 60, true)}</TitleText>
           <SubTitle>{address}</SubTitle>
-        </CardContainer>
-        <Pointer
+        </ThemedCardContainer>
+        <ThemedPointer
           style={hover ? { visibility: 'visible' } : { visibility: 'hidden' }}
         />
       </div>
