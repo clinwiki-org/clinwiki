0eac03da67539342d48c5a1f40c79f603ddc1754
Brian Espinosa <bespinosa1931@gmail.com>
2020-04-28 16:57:06 -0500

    adds the correct code in search service backk in. wrong code merged during merge conflict, caused wiki_page_edits filters to not apply correctly

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -144,30 +144,6 @@ class SearchService
   # Search results from params
   # @return [Hash] the JSON response
   def search(search_after: nil, reverse: false, includes: [])
-    options = search_options(search_after: search_after, reverse: reverse, includes: includes)
-    search_result = Study.search("*", options) { |body| enrich_body(body) }
-    {
-      recordsTotal: search_result.total_entries,
-      studies: search_result.results,
-      aggs: search_result.aggs,
-    }
-  end
-
-  def scroll
-    scroll_options = search_options.except(
-      :page, :per_page, :aggs
-    ).merge(scroll: "1m", load: false)
-
-    Study.search("*", scroll_options) do |body|
-      enrich_body(body)
-    end
-  end
-
-  def enrich_body(body)
-    body[:query][:bool][:must] = { query_string: { query: search_query } }
-  end
-
-  def search_options(search_after: nil, reverse: false, includes: [])
     crowd_aggs = agg_buckets_for_field(field: "front_matter_keys")
       &.dig(:front_matter_keys, :buckets)
       &.map { |bucket| "fm_#{bucket[:key]}" } || []
@@ -175,8 +151,16 @@ class SearchService
     aggs = (crowd_aggs + ENABLED_AGGS).map { |agg| [agg, { limit: 10 }] }.to_h
     options = search_kick_query_options(aggs: aggs, search_after: search_after, reverse: reverse)
     options[:includes] = includes
-    options
-  end
+    search_result = Study.search("*", options) do |body|	
+      body[:query][:bool][:must] = [{ query_string: { query: search_query } }]	
+      body[:query][:bool][:must] += nested_filters unless nested_filters.empty?	
+      body[:query][:bool][:must] += nested_range_filters unless nested_range_filters.empty?	
+    end	
+    {	
+      recordsTotal: search_result.total_entries,	
+      studies: search_result.results,	
+      aggs: search_result.aggs,	
+    }  end
 
   def agg_buckets_for_field(field:, current_site: nil, is_crowd_agg: false, url: nil, config_type: nil, return_all: false)
     params = self.params.deep_dup
