c496ccb037b7c72ec417d3839a1928098b101c8d
Tony Wade <wadant3@gmail.com>
2020-05-08 11:47:36 -0400

    Generate jwt if token is valid and password is valid

--- app/graphql/mutations/update_password.rb
+++ app/graphql/mutations/update_password.rb
@@ -1,7 +1,8 @@
 module Mutations
   class UpdatePassword < BaseMutation
     field :user, Types::UserType, null: false
-    field :errors, String, null:false
+    field :errors, String,"Errors with token, either does not match user or expired", null:false
+    field :jwt, String,"Sign in token if no error", null:true
 
     argument :reset_password_token, String, required: true
     argument :password, String, required: true
@@ -9,10 +10,20 @@ module Mutations
 
     def resolve(attrs)
       user = User.reset_password_by_token(attrs)
+      jwt = user.errors.full_messages.empty? ? jwt(user.email)  : nil
       {
         user: user,
-        errors: user.errors.full_messages
+        errors: user.errors.full_messages,
+        jwt: jwt
       }
+    end
+
+
+    def jwt(email)
+      hmac_secret = Rails.application.secrets.secret_key_base
+      exp_secs = ENV["JWT_EXPIRATION_TIME_SECS"] || 86_400
+      exp = Time.now.to_i + exp_secs.to_i
+      JWT.encode({ email: email, exp: exp }, hmac_secret, "HS256")
     end
   end
 end
