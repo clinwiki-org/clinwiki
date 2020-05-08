6d6fb48086ea00fb4877185b0b9d356dca72575b
Tony Wade <wadant3@gmail.com>
2020-05-08 11:15:50 -0400

    default json string for ranking

--- db/schema.rb
+++ db/schema.rb
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_183458) do
     t.datetime "updated_at", null: false
     t.boolean "skip_landing"
     t.text "themes", default: "{\"primaryColor\":\"#6BA5D6\",\"secondaryColor\":\"#1b2a38\",\"lightTextColor\":\"#eee\",\"secondaryTextColor\":\"#333\",\"backgroundColor\":\"#4D5863\",\"primaryAltColor\":\"#5786AD\",\"authHeaderColor\":\"#5786AD\",\"sideBarColor\":\"#4d5762\"} "
+    t.text "user_rank", default: "{\"default\":{\"gte\":0},\"bronze\":{\"gte\":26},\"silver\":{\"gte\":51},\"gold\":{\"gte\":75},\"platinum\":{\"gte\":101}} "
     t.index ["subdomain"], name: "index_sites_on_subdomain", unique: true
   end
 
