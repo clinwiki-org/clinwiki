20d53f18bd90ff2d9db8a365b0ac262c98904e1b
Brian Espinosa <bespinosa1931@gmail.com>
2020-05-22 10:08:20 -0500

    additional cleanup

--- front/app/containers/AggDropDown/AggDropDown.tsx
+++ front/app/containers/AggDropDown/AggDropDown.tsx
@@ -503,13 +503,6 @@ class AggDropDown extends React.Component<AggDropDownProps, AggDropDownState> {
     } = this.state;
     const field = findFields(agg, currentSiteView, presearch);
 
-    const data = [
-      { name: 'Group A', value: 400 },
-      { name: 'Group B', value: 300 },
-      { name: 'Group C', value: 300 },
-      { name: 'Group D', value: 200 },
-    ];
-
     if (
       field?.display === FieldDisplay.DATE_RANGE ||
       field?.display === FieldDisplay.NUMBER_RANGE ||
