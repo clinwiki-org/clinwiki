326e0499b45e14576fa078fd5470ba7edf1bcbee
Brian Espinosa <bespinosa1931@gmail.com>
2020-06-03 10:44:02 -0500

    initial working of reactions

--- front/src/containers/StudyPage/components/StudyPageHeader.tsx
+++ front/src/containers/StudyPage/components/StudyPageHeader.tsx
@@ -3,20 +3,24 @@ import styled from 'styled-components';
 import * as FontAwesome from 'react-fontawesome';
 import ReactStars from 'react-stars';
 import ThemedButton from 'components/StyledComponents';
-import {getStarColor} from '../../../utils/auth'
+import { getStarColor } from '../../../utils/auth'
+import { GithubSelector, GithubCounter, SlackCounter, SlackSelector } from 'react-reactions';
 import { StudyPageQuery, StudyPageQueryVariables } from 'types/StudyPageQuery';
 
 interface StudyPageHeaderProps {
 
-    navButtonClick:any;
+    navButtonClick: any;
     user: any;
-    history:any;
-    data:any;
-    theme:any;
+    history: any;
+    data: any;
+    theme: any;
 }
-interface StudyPageHeaderState{
+interface StudyPageHeaderState {
     like: boolean;
-    dislike:boolean;
+    dislike: boolean;
+    showReactions: boolean;
+    reactions: any;
+    counters: any;
 }
 const ReviewsWrapper = styled.div`
   display: flex;
@@ -46,6 +50,24 @@ const ThumbsRow = styled.div`
   margin: 3px;
   flex-direction: row;
   display: flex;
+    span > div> div>div{
+            background: none !important;
+            height: 19px;
+            padding-top: 1px;
+            padding-left: 3px;
+            padding-right: 4px;
+            border: 1px solid rgb(187, 225, 255);
+            font-size: 11px;
+            color: rgb(153, 153, 153);
+            font-weight: 500;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            position: relative;
+            cursor: pointer;
+            border-radius: 5px
+          
+    }
 `;
 
 const ThumbIcon = styled.div`
@@ -69,142 +91,173 @@ const LikesText = styled.div`
 
 // A simple counter that displays which study you're on on the study page, in the middle of the prev and next buttons
 class StudyPageHeader extends React.Component<StudyPageHeaderProps, StudyPageHeaderState> {
-    state:StudyPageHeaderState={
+    state: StudyPageHeaderState = {
         like: false,
-        dislike:false
+        dislike: false,
+        showReactions: false,
+        reactions: [],
+        counters: []
+
     }
     renderBackButton = (name: string, link?: string | null) => {
         if (link === undefined) return null;
-    
+
         return (
-          <div style={{ paddingTop: '10px' }}>
-            <ThemedButton
-              style={{ margin: 'auto', float: 'left' }}
-              onClick={this.props.navButtonClick(link!)}
-              disabled={link === null}>
-              {name}
-            </ThemedButton>
-          </div>
+            <div style={{ paddingTop: '10px' }}>
+                <ThemedButton
+                    style={{ margin: 'auto', float: 'left' }}
+                    onClick={this.props.navButtonClick(link!)}
+                    disabled={link === null}>
+                    {name}
+                </ThemedButton>
+            </div>
         );
-      };
+    };
 
-      thumbsUpClick = () => {
+    thumbsUpClick = () => {
         if (this.state.like) {
-          this.setState({
-            like: false,
-          });
+            this.setState({
+                like: false,
+            });
         } else {
-          this.setState({
-            like: true,
-            dislike: false,
-          });
+            this.setState({
+                like: true,
+                dislike: false,
+            });
         }
-      };
-    
-      thumbsDownClick = () => {
+    };
+
+    thumbsDownClick = () => {
         if (this.state.dislike) {
-          this.setState({
-            dislike: false,
-          });
+            this.setState({
+                dislike: false,
+            });
         } else {
-          this.setState({
-            dislike: true,
-            like: false,
-          });
+            this.setState({
+                dislike: true,
+                like: false,
+            });
         }
-      };
+    };
 
+    componentDidMount = () => {
+        let reactions: any[] = [];
+        reactions = ['👍', '👎', '❤️', '☠️']
 
-      renderReviewsSummary = (data: StudyPageQuery | undefined) => {
+        let counters: any[] = [];
+
+        counters = [{
+            emoji: '👍', // String emoji reaction
+            by: 'Brian', // String of persons name
+        },
+        {
+            emoji: '👎', // String emoji reaction
+            by: 'Brian', // String of persons name
+        },
+        {
+            emoji: '❤️', // String emoji reaction
+            by: 'Brian', // String of persons name
+        },
+        {
+            emoji: '☠️', // String emoji reaction
+            by: 'Brian', // String of persons name
+        }]
+        this.setState({ reactions: reactions, counters: counters })
+    }
+    renderReviewsSummary = (data: StudyPageQuery | undefined) => {
         const { theme } = this.props;
         if (!data || !data.study) {
-          return (
-            <ReviewsWrapper>
-              <div>
-                <ReactStars
-                  count={5}
-                  color2={theme.studyPage.reviewStarColor}
-                  edit={false}
-                  value={0}
-                />
-                <div>{'0 Reviews'}</div>
-              </div>
-            </ReviewsWrapper>
-          );
+            return (
+                <ReviewsWrapper>
+                    <div>
+                        <ReactStars
+                            count={5}
+                            color2={theme.studyPage.reviewStarColor}
+                            edit={false}
+                            value={0}
+                        />
+                        <div>{'0 Reviews'}</div>
+                    </div>
+                </ReviewsWrapper>
+            );
         }
-    
-        return (
-          <ReviewsWrapper>
-            <div>
-              <ReactStars
-                count={5}
-                color2={theme.studyPage.reviewStarColor}
-                edit={false}
-                value={data.study.averageRating}
-              />
-              <div
-                style={{
-                  color: 'rgba(255, 255, 255, 0.5)',
-                }}>{`${data.study.reviewsCount} Reviews`}</div>
-            </div>
-          </ReviewsWrapper>
-        );
-      };
 
-  render() {
-    const hash = new URLSearchParams(this.props.history.location.search)
-    .getAll('hash')
-    .toString();
-  const siteViewUrl = new URLSearchParams(this.props.history.location.search)
-    .getAll('sv')
-    .toString();
-  const userRank = this.props.user ? this.props.user.rank : 'default'
-  let rankColor = getStarColor(userRank)
-  const backLink = () => {
-    if (hash !== '') {
-      return `/search?hash=${hash}&sv=${siteViewUrl}`;
+        return (
+            <ReviewsWrapper>
+                <div>
+                    <ReactStars
+                        count={5}
+                        color2={theme.studyPage.reviewStarColor}
+                        edit={false}
+                        value={data.study.averageRating}
+                    />
+                    <div
+                        style={{
+                            color: 'rgba(255, 255, 255, 0.5)',
+                        }}>{`${data.study.reviewsCount} Reviews`}</div>
+                </div>
+            </ReviewsWrapper>
+        );
+    };
+    handleEmojiSelect = (e) => {
+        console.log(e)
     }
-    return undefined;
-  };
-    return (
-        <HeaderContentWrapper>
-          <BackButtonContainer>
-            {this.renderBackButton('⤺︎ Back', backLink())}
-          </BackButtonContainer>
-          <ReactionsContainer>
-            <LikesRow>
-              <ThumbsRow>
-                <ThumbIcon>
-                  <FontAwesome
-                    name="thumbs-up"
-                    style={{
-                      color: this.state.like ? 'green' : '#fff',
-                      fontSize: 24,
-                    }}
-                    onClick={this.thumbsUpClick}
-                  />
-                </ThumbIcon>
-                <LikesText>120</LikesText>
-              </ThumbsRow>
-              <ThumbsRow>
-                <ThumbIcon>
-                  <FontAwesome
-                    name="thumbs-down"
-                    style={{
-                      color: this.state.dislike ? 'red' : '#fff',
-                      fontSize: 24,
-                    }}
-                    onClick={this.thumbsDownClick}
-                  />
-                </ThumbIcon>
-                <LikesText style={{ color: '#B8B8B8' }}>20</LikesText>
-              </ThumbsRow>
-            </LikesRow>
-            {this.renderReviewsSummary(this.props.data)}
-          </ReactionsContainer>
-        </HeaderContentWrapper>
-    );
-  }
+    handleAddReaction = (e) => {
+        console.log("Clicked", e)
+        this.setState({ showReactions: !this.state.showReactions })
+
+    }
+    handleSelectorClick=(e)=>{
+        console.log("CLICK CLACK", e)
+        let newObject = {
+            emoji: `${e}`,
+            by: `${this.props.user.email}`
+        }
+        console.log("NEW",newObject)
+        let oldArray= this.state.counters
+        console.log('OLD', oldArray)
+
+       oldArray.push(newObject)
+        this.setState({counters: oldArray, showReactions:false })
+    }
+    render() {
+
+        const hash = new URLSearchParams(this.props.history.location.search)
+            .getAll('hash')
+            .toString();
+        const siteViewUrl = new URLSearchParams(this.props.history.location.search)
+            .getAll('sv')
+            .toString();
+        const userRank = this.props.user ? this.props.user.rank : 'default'
+        let rankColor = getStarColor(userRank)
+        const backLink = () => {
+            if (hash !== '') {
+                return `/search?hash=${hash}&sv=${siteViewUrl}`;
+            }
+            return undefined;
+        };
+        return (
+            <HeaderContentWrapper>
+                <BackButtonContainer>
+                    {this.renderBackButton('⤺︎ Back', backLink())}
+                </BackButtonContainer>
+                <ReactionsContainer>
+                    <LikesRow>
+                        <ThumbsRow>
+                            <SlackCounter
+                                counters={this.state.counters}
+                                user={"Brian"}
+                                onSelect={this.handleEmojiSelect}
+                                onAdd={this.handleAddReaction}
+                            />
+                            {this.state.showReactions == true ? <GithubSelector reactions={this.state.reactions} onSelect={this.handleSelectorClick} /> : null}
+                        </ThumbsRow>
+                    </LikesRow>
+                    {this.renderReviewsSummary(this.props.data)}
+                </ReactionsContainer>
+            </HeaderContentWrapper>
+        );
+    }
 }
 
 export default StudyPageHeader;
