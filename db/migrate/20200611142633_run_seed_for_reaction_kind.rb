977bbacf85b06749f7661ad13e8cb04bfc813552
Tony Wade <wadant3@gmail.com>
2020-06-11 14:25:41 -0400

    migration to add reaction kind and to remove the ones created if you rollback migration

--- db/migrate/20200611142633_run_seed_for_reaction_kind.rb
+++ db/migrate/20200611142633_run_seed_for_reaction_kind.rb
@@ -1,6 +1,15 @@
 class RunSeedForReactionKind < ActiveRecord::Migration[5.2]
   #Migration runs rake db:seed essentially
-  def change
-    Rails.application.load_seed
+  def up
+    ReactionKind.find_or_initialize_by(name:"like").update(name:"like")
+    ReactionKind.find_or_initialize_by(name:"dislike").update(name:"dislike")
+    ReactionKind.find_or_initialize_by(name:"heart").update(name:"heart")
+    ReactionKind.find_or_initialize_by(name:"skull_and_cross_bones").update(name:"skull_and_cross_bones")
+  end
+  def down
+    ReactionKind.find_by(name:"like").destroy
+    ReactionKind.find_by(name:"dislike").destroy
+    ReactionKind.find_by(name:"heart").destroy
+    ReactionKind.find_by(name:"skull_and_cross_bones").destroy
   end
 end
