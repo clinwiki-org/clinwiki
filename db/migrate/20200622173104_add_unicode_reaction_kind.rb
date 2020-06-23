3d9323fea074e04cdb20d4e007e64af17f956461
Tony Wade <wadant3@gmail.com>
2020-06-23 15:13:34 -0400

    Unicode correct encoding

--- db/migrate/20200622173104_add_unicode_reaction_kind.rb
+++ db/migrate/20200622173104_add_unicode_reaction_kind.rb
@@ -1,10 +1,10 @@
 class AddUnicodeReactionKind < ActiveRecord::Migration[5.2]
   def up
     add_column :reaction_kinds, :unicode, :string
-    ReactionKind.find_or_initialize_by(name:"like").update(unicode: "\\u1F44D")
-    ReactionKind.find_or_initialize_by(name:"dislike").update(unicode:"\\u1F44E")
-    ReactionKind.find_or_initialize_by(name:"heart").update(unicode:"\\u2764")
-    ReactionKind.find_or_initialize_by(name:"skull_and_cross_bones").update(unicode:"\\u2620")
+    ReactionKind.find_or_initialize_by(name:"like").update(unicode: "1F44D".hex.chr("utf-8"))
+    ReactionKind.find_or_initialize_by(name:"dislike").update(unicode:"1F44E".hex.chr("utf-8"))
+    ReactionKind.find_or_initialize_by(name:"heart").update(unicode:"2764".hex.chr("utf-8"))
+    ReactionKind.find_or_initialize_by(name:"skull_and_cross_bones").update(unicode:"2620".hex.chr("utf-8"))
   end
 
   def down
