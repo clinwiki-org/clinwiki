4fcfc03ccb96ba47c206403b54ba0f5b4391e028
Brian Espinosa <bespinosa1931@gmail.com>
2020-07-02 10:31:03 -0500

    pull color for reactions buttons from theme

--- front/src/components/ReactionsBar/SlackCounter/SlackCounter.tsx
+++ front/src/components/ReactionsBar/SlackCounter/SlackCounter.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 import _ from 'lodash'
 import styled from 'styled-components';
 import SlackCounterGroup from './SlackCounterGroup'
-import { find, propEq, findLastIndex, filter } from 'ramda';
+import { find, propEq } from 'ramda';
 import { Icon, InlineIcon } from '@iconify/react';
 import smilePlus from '@iconify/icons-fe/smile-plus';
 import DeleteReactionMutation, {
 } from 'mutations/DeleteReactionMutation';
 import CreateReactionMutation, {
 } from 'mutations/CreateReactionMutation';
+import withTheme from 'containers/ThemeProvider/ThemeProvider';
 
 interface SlackCounterProps {
     activeReactions: any;
@@ -20,13 +21,13 @@ interface SlackCounterProps {
     currentUserAndStudy: any;
     studyRefetch?: any;
     refetch?: any;
-    allReactions:any;
+    allReactions: any;
 }
 interface SlackCounterState {
     showLabel: boolean;
     currentUserAndStudy: any[];
 }
-const Counter = styled.div`
+const StyledCounter = styled.div`
     display: flex;
 
     .add{
@@ -37,8 +38,8 @@ const Counter = styled.div`
         display: flex;
         margin-top: auto;
         margin-bottom: auto;
-        background: #6BA5D6;
-        border: 1px solid #5786AD;
+        background: ${props => props.theme.button};
+        border: 1px solid ${props => props.theme.button};
         border-radius: 5px;
 
     }
@@ -47,8 +48,8 @@ const Counter = styled.div`
         display: flex;
         margin-top: auto;
         margin-bottom: auto;
-        background: #6BA5D6;
-        border: 1px solid #5786AD;
+        background: ${props => props.theme.button};
+        border: 1px solid ${props => props.theme.button};
         border-radius: 5px;
         cursor:pointer;
     }
@@ -58,11 +59,13 @@ const Counter = styled.div`
         margin-top: auto;
         margin-bottom: auto;
         background: transparent;
-        border: 1px solid #5786AD;
+        border: 1px solid ${props => props.theme.button};
         border-radius: 5px;
         cursor:pointer;
     }
 `
+
+export const ThemedCounter = withTheme(StyledCounter);
 class SlackCounter extends React.Component<SlackCounterProps, SlackCounterState> {
     state: SlackCounterState = {
         showLabel: false,
@@ -101,29 +104,29 @@ class SlackCounter extends React.Component<SlackCounterProps, SlackCounterState>
 
 
     }
-    currentReactionFilter=(reactionName)=>{
+    currentReactionFilter = (reactionName) => {
         //we dont have all the necessary data in activeReactions to interact with the db, 
         //this is where this function comes into play:
 
         //we take the reaction name and find it in our array with all our reactions that has all that data
 
-        if(this.props.allReactions.data){
+        if (this.props.allReactions.data) {
             let allReactions = this.props.allReactions.data.reactionKinds
-             return find(propEq('name', reactionName))(allReactions)
+            return find(propEq('name', reactionName))(allReactions)
         }
     }
-    findUserReaction = (reaction: any |undefined, userReactions: any[]): object | undefined=>{
+    findUserReaction = (reaction: any | undefined, userReactions: any[]): object | undefined => {
         //much like currentReactionFilter() we don't have the necesarry data 
         //to interact with db  in the case a reaction is one a user has already interacted with before
-        
+
 
         //in order to make sure we are passing the adequate data needed to update our db we look for it in our array userReactions where we can find everything we need
-         if (reaction && userReactions){
-         return find(propEq('reactionKindId', reaction.id))(userReactions);
-     
-     }
-     return
-     }
+        if (reaction && userReactions) {
+            return find(propEq('reactionKindId', reaction.id))(userReactions);
+
+        }
+        return
+    }
     renderReactionButtons = () => {
         let userReactionsCurrent = this.state.currentUserAndStudy;
         return (
@@ -183,7 +186,7 @@ class SlackCounter extends React.Component<SlackCounterProps, SlackCounterState>
         let addEmoji = <Icon icon={smilePlus} width="1.5em" />
         let userReactionsCurrent = this.state.currentUserAndStudy;
         return (
-            <Counter>
+            <ThemedCounter>
 
                 {this.renderReactionButtons()}
                 <div className="add" onClick={this.props.onAdd}>
@@ -197,7 +200,7 @@ class SlackCounter extends React.Component<SlackCounterProps, SlackCounterState>
                     />
 
                 </div>
-            </Counter>
+            </ThemedCounter>
         )
     }
 
