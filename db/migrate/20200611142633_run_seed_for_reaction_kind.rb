4c9fc87d5bb4e4c9232b46b15a6227ee59e8bb5d
Tony Wade <wadant3@gmail.com>
2020-07-09 10:26:50 -0400

    Removed commented code from migration

--- db/migrate/20200611142633_run_seed_for_reaction_kind.rb
+++ db/migrate/20200611142633_run_seed_for_reaction_kind.rb
@@ -1,15 +1,7 @@
 class RunSeedForReactionKind < ActiveRecord::Migration[5.2]
-  #Migration runs rake db:seed essentially
+  #Previously data manipulation but broke migrations on production
   def up
-    # ReactionKind.find_or_initialize_by(name:"like").update(name:"like")
-    # ReactionKind.find_or_initialize_by(name:"dislike").update(name:"dislike")
-    # ReactionKind.find_or_initialize_by(name:"heart").update(name:"heart")
-    # ReactionKind.find_or_initialize_by(name:"skull_and_cross_bones").update(name:"skull_and_cross_bones")
   end
   def down
-    # ReactionKind.find_by(name:"like").destroy
-    # ReactionKind.find_by(name:"dislike").destroy
-    # ReactionKind.find_by(name:"heart").destroy
-    # ReactionKind.find_by(name:"skull_and_cross_bones").destroy
   end
 end
