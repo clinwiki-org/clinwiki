b51cc887199a56597313130d1908d9f2dc4f3112
Tony Wade <wadant3@gmail.com>
2020-07-09 13:30:46 -0400

    Code added for site query to get just one pageview

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -2,6 +2,7 @@ class PageView < ApplicationRecord
   belongs_to :site
   validates :site, presence: true
   enum page_type: [ :study ]
+  validates :url, uniqueness: { scope: :site }
   include MutationHelpers
 
 
