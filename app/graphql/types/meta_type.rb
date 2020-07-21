2d655ce315232e362b8e64092639ddcab70ed609
Tony Wade <wadant3@gmail.com>
2020-07-21 10:59:45 -0400

    metatype same level crowdKey and crowdValue

---
+++ app/graphql/types/meta_type.rb
@@ -0,0 +1,14 @@
+module Types
+  class MetaType < BaseObject
+    field :crowd_key, String, null: false
+    field :crowd_value, [String], null: true
+
+
+    def key
+      object[:crowd_key]
+    end
+    def value
+      object[:crowd_value]
+    end
+  end
+end
