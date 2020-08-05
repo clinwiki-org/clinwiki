4b424f4b918351d9f3ab788958b01f1fc8e6a2bc
Tony <wadant3@gmail.com>
2020-08-05 09:21:05 -0400

    Set elastic password as enviroment variable so not plain text in github, also reenabled kibana but gave it a username and password

--- db/schema.rb
+++ db/schema.rb
@@ -143,7 +143,7 @@ ActiveRecord::Schema.define(version: 2020_07_24_182046) do
     t.boolean "skip_landing"
     t.text "themes", default: "{\"primaryColor\":\"#6BA5D6\",\"secondaryColor\":\"#1b2a38\",\"lightTextColor\":\"#eee\",\"secondaryTextColor\":\"#333\",\"backgroundColor\":\"#4D5863\",\"primaryAltColor\":\"#5786AD\",\"authHeaderColor\":\"#5786AD\",\"sideBarColor\":\"#4d5762\"} "
     t.text "user_rank", default: "[{\"rank\":\"default\",\"gte\":0},{\"rank\":\"bronze\",\"gte\":26},{\"rank\":\"silver\",\"gte\":51},{\"rank\":\"gold\",\"gte\":75},{\"rank\":\"platinum\",\"gte\":101}] "
-    t.text "reactions_config", default: "[{\"name\":\"like\"},{\"name\":\"dislike\"},{\"name\":\"heart\"},{\"name\":\"skull_and_cross_bones\"}]"
+    t.text "reactions_config", default: "[]"
     t.index ["subdomain"], name: "index_sites_on_subdomain", unique: true
   end
 
