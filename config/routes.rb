9221138ad4fa54bd90d70a3c7b65c5b61c14f89f
Kaveer Kapoor <y2kaveer@gmail.com>
2020-08-26 14:54:15 -0700

    Change loader.io domain verification key for als.clinwiki.org since that is the main concern for ALS subsite launch.

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   end
 
   # loader.io domain verifcation for load testing
-  get "/loaderio-f076138abbb7fbc078ad112a429b504f.txt", to: static("loaderio-f076138abbb7fbc078ad112a429b504f.txt")
+  get "/loaderio-21df9bee0c48bbdb7696ea087e1c5492.txt", to: static("loaderio-21df9bee0c48bbdb7696ea087e1c5492.txt")
 
   match "*all", to: static("index.html"), via: [:get]
 end
