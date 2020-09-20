3ef39ed6c55ba2f4b7ffdd24ba7c07dedce54357
Kaveer <kaveer@clinwiki.org>
2020-09-19 23:58:33 -0700

    Prettier auto format on save

--- front/src/containers/Islands/ReactionsIsland.tsx
+++ front/src/containers/Islands/ReactionsIsland.tsx
@@ -1,4 +1,3 @@
-
 import React, { useContext } from 'react';
 import { Panel } from 'react-bootstrap';
 import styled from 'styled-components';
@@ -10,7 +9,7 @@ import { useCurrentUser } from 'containers/CurrentUser/CurrentUser';
 import QUERY from 'queries/ReactionsIslandQuery';
 import { useQuery } from 'react-apollo';
 import REACTION_KINDS from 'queries/ReactionKinds';
-import ReactionsBar from '../../components/ReactionsBar'
+import ReactionsBar from '../../components/ReactionsBar';
 
 interface Props {
   nctId?: string;
@@ -21,7 +20,7 @@ const StyledPanel = styled(Panel)`
 export default function ReactionsIsland(props: Props) {
   const { nctId } = props;
   const { site } = useSite();
-  const theme  = useTheme();
+  const theme = useTheme();
   const user = useCurrentUser()?.data?.me;
   // TODO: This query should be pushed up as a fragment to the Page
   const { data: studyData } = useQuery<ReactionsIslandQuery>(QUERY, {
@@ -30,17 +29,17 @@ export default function ReactionsIsland(props: Props) {
   const { data: allReactions } = useQuery<ReactionKinds>(REACTION_KINDS, {
     variables: { nctId },
   });
-if(site && allReactions && studyData && theme){
-  return (
+  if (site && allReactions && studyData && theme) {
+    return (
       <ReactionsBar
-          reactionsConfig={site.reactionsConfig}
-          nctId={nctId}
-          theme={theme}
-          studyData={studyData.study}
-          user={user}
-          allReactions={allReactions}
+        reactionsConfig={site.reactionsConfig}
+        nctId={nctId}
+        theme={theme}
+        studyData={studyData.study}
+        user={user}
+        allReactions={allReactions}
       />
-  );
-}
-return null;
+    );
+  }
+  return null;
 }
