c3be0f022b385ff8872da3fd3c18c9d3ddd7333d
bespinosa <bespinosa1931@gmail.com>
2020-09-30 12:13:17 -0500

    cleanup comment

--- front/src/hooks/useUnload.ts
+++ front/src/hooks/useUnload.ts
@@ -15,20 +15,5 @@ export function useUnload(fn) {
       return () => window.removeEventListener("beforeunload", onUnload);
     }, []);
   }
-// export function useUnload = (fn) => {
-//   const cb = useRef(fn); // init with fn, so that type checkers won't assume that current might be undefined
-
-//   useEffect(() => {
-//     cb.current = fn;
-//   }, [fn]);
-
-//   useEffect(() => {
-//     const onUnload = cb.current;
-
-//     window.addEventListener("beforeunload", onUnload);
-
-//     return () => window.removeEventListener("beforeunload", onUnload);
-//   }, []);
-// };
 
 export default useUnload;
