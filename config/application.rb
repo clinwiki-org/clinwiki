5bb6b5cd8c866e1d7aa7fc44161583c86e206e40
Eric Harding <eharding@blackedge.com>
2020-09-29 20:36:22 -0400

    Gzip js

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,8 @@ module Clinwiki
           :credentials => true
       end
     end
-    config.middleware.insert_before 1,  Rack::Redirect
+    config.middleware.insert_before 1, Rack::Redirect
+    config.middleware.insert_before 2, Rack::Deflater
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
