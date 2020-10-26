6fdd3bfc219649b22eb3a00f54fc15f3431f6b20
Rod Chavez <rodctech@gmail.com>
2020-10-26 16:27:27 -0500

    Adds Less/Greater than range functionality to drop down, TODO in 104

--- front/src/containers/AggDropDown/BucketsDropDownOptions.tsx
+++ front/src/containers/AggDropDown/BucketsDropDownOptions.tsx
@@ -63,22 +63,13 @@ class BucketsDropDownOptions extends React.Component<
 
     let activeOptions: string[] = [];
 
-    const changeDropDownRange = async e => {
-      e.preventDefault();
-
-      this.setState({
-        ...this.state,
-        endText: e.target.value,
-      })
-    /*   this.setState(
-        { ...this.state, start: this.state.startText, end: this.state.endText },
-        this.onChange
-      ); */
-
-     /*  activeOptions.forEach(o => {
-        updater.removeFilter(o);
-      });
-      updater.toggleFilter(e.target.value); */
+    const changeDropDownRange = e => {
+      //e.preventDefault();
+      if (display === 'LESS_THAN_DROP_DOWN') {
+        this.setState({ ...this.state, endText:e.target.value, end: e.target.value }, this.onChange);
+      } else {
+        this.setState({ ...this.state, startText:e.target.value, start: e.target.value }, this.onChange);
+      }
     };
 
     const changeDropDownOption = async e => {
@@ -96,32 +87,34 @@ class BucketsDropDownOptions extends React.Component<
       //console.log("active", activeOptions)
       return (
         <option value={bucket.key}>
-          {defaultTo(bucket.key)(bucket.keyAsString)} ({bucket.docCount})
+          {defaultTo(bucket.key)(bucket.keyAsString)}{' '}
+          {display === 'DROP_DOWN' ? bucket.docCount : null}
         </option>
       );
     };
 
     // console.log("Active Opt", (activeOptions.length !== 0))
-    console.log('Active Opt', activeOptions);
+    //console.log('Active Opt', activeOptions);
     const selectedOption =
       activeOptions === undefined || activeOptions.length === 0
         ? 'Select Option'
         : activeOptions;
-
     // console.log("!!!!!!!!!!", selectedOption)
+
+    //TODO Needs default value text when no option is selected on drop down yet, could use state
+    // ! when filter is removed from top with "x". Dropdown default value doesn't reflect change.
 
     if (field?.display === FieldDisplay.DROP_DOWN) {
       return (
         <div className="dropDownFacet">
           <StyledFormControl
             //multiple
-            // name={activeOptions[0]}
             componentClass={'select'}
-            //defaultValue={"Option"}
             value={-1}
+            //defaultValue={"Option"}
             //placeholder={"Options"}
             onChange={e => changeDropDownOption(e)}>
-            <option value={-1} key={-1}>
+            <option disabled value={-1} key={-1}>
               {activeOptions}
             </option>
             {buckets
@@ -141,27 +134,19 @@ class BucketsDropDownOptions extends React.Component<
       field?.display === FieldDisplay.GREATER_THAN_DROP_DOWN
     ) {
       const { startText, endText } = this.state;
+      const rangeValue = display === 'LESS_THAN_DROP_DOWN' ? endText : startText;
       const dropDownLabel =
         display === 'LESS_THAN_DROP_DOWN' ? 'Less Than' : 'Greater Than';
-
-      //console.log('Display', display, 'FIELD', field);
       return (
         <div className="dropDownFacet">
           <ControlLabel>{dropDownLabel}</ControlLabel>
           <StyledFormControl
-            //multiple
-            // name={activeOptions[0]}
             componentClass={'select'}
+            value={-1}
             //defaultValue={"Option"}
-            value={endText}
-            //placeholder={"Options"}
-            onChange={e => changeDropDownRange(e)}
-            onBlur={() =>
-                this.setState({ ...this.state, end: endText }, this.onChange)
-            }
-            >
-            <option value={-1} key={-1}>
-              {activeOptions}
+            onChange={e => changeDropDownRange(e)}>
+            <option disabled value={-1} key={-1}>
+              {rangeValue}
             </option>
             {buckets
               .filter(
