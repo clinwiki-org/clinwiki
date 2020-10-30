21769b054c8395007d53758acadfcebce4efc207
Rod Chavez <rodctech@gmail.com>
2020-10-30 10:37:06 -0500

    Makes preselected values & visible options on siteview config aggs show as checkboxes

--- front/src/containers/AggDropDown/AggDropDown.tsx
+++ front/src/containers/AggDropDown/AggDropDown.tsx
@@ -137,6 +137,7 @@ interface AggDropDownProps {
   client: ApolloClient<any>;
   site: PresentSiteFragment;
   presentSiteView: PresentSiteFragment_siteView;
+  fromAggField: boolean;
 }
 
 class AggDropDown extends React.Component<AggDropDownProps, AggDropDownState> {
@@ -422,9 +423,10 @@ class AggDropDown extends React.Component<AggDropDownProps, AggDropDownState> {
       );
     }
     else if (
-      field?.display === FieldDisplay.DROP_DOWN ||
+      (field?.display === FieldDisplay.DROP_DOWN ||
       field?.display === FieldDisplay.LESS_THAN_DROP_DOWN ||
-      field?.display === FieldDisplay.GREATER_THAN_DROP_DOWN
+      field?.display === FieldDisplay.GREATER_THAN_DROP_DOWN)
+      && !this.props.fromAggField
     ){
       return (
       <Panel.Collapse className="bm-panel-collapse">
