afba478f262e94c9a2458f0575c900596577b171
Rod Chavez <rodctech@gmail.com>
2020-11-10 10:05:45 -0600

    rollsback search_logs table fields

--- db/migrate/20201030190803_add_user_history_fields_to_search_logs.rb
+++
@@ -1,8 +0,0 @@
-class AddUserHistoryFieldsToSearchLogs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :search_logs, :is_saved, :boolean, default: false, null:false
-    add_column :search_logs, :is_subscribed, :boolean, default: false, null:false
-    add_column :search_logs, :name_default, :string
-    add_column :search_logs, :name_label, :string
-  end
-end
