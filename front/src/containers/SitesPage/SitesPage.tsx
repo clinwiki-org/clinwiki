fb5ae6f45b7ada01f1105f0cad94f0a56b586de2
Rod Chavez <rodctech@gmail.com>
2020-12-23 14:30:25 -0600

    SItesPage to functional component implements redux, Not returning data from query

--- front/src/containers/SitesPage/SitesPage.tsx
+++ front/src/containers/SitesPage/SitesPage.tsx
@@ -11,41 +11,130 @@ import DeleteSiteMutation, {
   DeleteSiteMutationFn,
 } from 'mutations/DeleteSiteMutations';
 import ThemedButton from 'components/StyledComponents/index';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSitesPage } from 'services/site/actions'
+import { useEffect } from 'react';
+import {RootState} from 'reducers';
 
+
+
+
+const Container = styled.div`
+padding: 20px;
+`;
+
+const ButtonsContainer = styled.div`
+display: flex;
+justify-content: flex-end;
+margin-bottom: 20px;
+`;
 interface SitesPageProps {
   history: History;
 }
 
-const QUERY = gql`
-  query SitesPageQuery {
-    me {
-      id
-      ownSites {
-        ...SiteItemFragment
-      }
-      editorSites {
-        ...SiteItemFragment
-      }
-    }
-  }
 
-  ${SiteItem.fragment}
-`;
 
-const Container = styled.div`
-  padding: 20px;
-`;
+const SitesPage = ({history} : SitesPageProps) => {
 
-const ButtonsContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 20px;
-`;
+  const dispatch = useDispatch();
+  //const user = useSelector( (state : RootState)  => state.user.current);
 
+  useEffect(() => {
+    dispatch(fetchSitesPage());
+  },[dispatch ]); //, user]);
+
+  const data = useSelector((state : RootState ) => state)
+
+  console.log("REDUX STATE ", data)
+
+
+  const handleCreateSite = () => {
+    history.push('/sites/new');
+  };
+
+  const handleSiteEdit = (id: number) => {
+    history.push(`/sites/${id}/edit`);
+  };
+
+  const handleSiteDelete = (deleteSite: DeleteSiteMutationFn) => (id: number) => {  //! We moved queries into the redux dir, what about mutations?
+    deleteSite({ variables: { input: { id } } });
+  };
+
+
+  return (
+    <Container>
+      <CollapsiblePanel header="My Sites">
+        {data.me.ownSites.length > 0 && (
+          <Table striped bordered condensed>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Subdomain</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              <DeleteSiteMutation>
+                {deleteSite => (
+                  <>
+                    {data!.me!.ownSites.map(site => (
+                      <SiteItem
+                        site={site}
+                        key={site.subdomain}
+                        onEdit={handleSiteEdit}
+                        onDelete={handleSiteDelete(deleteSite)}
+                      />
+                    ))}
+                  </>
+                )}
+              </DeleteSiteMutation>
+            </tbody>
+          </Table>
+        )}
+        {data.me.ownSites.length === 0 && 'No sites yet'}
+      </CollapsiblePanel>
+      <ButtonsContainer>
+        <ThemedButton onClick={handleCreateSite}>
+          Create Site
+        </ThemedButton>
+      </ButtonsContainer>
+      <CollapsiblePanel header="Editable Sites">
+        {data.me.editorSites.length > 0 && (
+          <Table striped bordered condensed>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Subdomain</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.me.editorSites.map(site => (
+                <SiteItem
+                  site={site}
+                  key={site.subdomain}
+                  onEdit={handleSiteEdit}
+                />
+              ))}
+            </tbody>
+          </Table>
+        )}
+        {data.me.editorSites.length === 0 && 'No sites yet'}
+      </CollapsiblePanel>
+    </Container>
+  );
+
+
+}
+
+
+export default SitesPage;
+
+
+/*
 const QueryComponent = (props: QueryComponentOptions<SitesPageQuery>) =>
   Query(props);
 
-class SitesPage extends React.PureComponent<SitesPageProps> {
+ class SitesPageOLD extends React.PureComponent<SitesPageProps> {
   handleCreateSite = () => {
     this.props.history.push('/sites/new');
   };
@@ -130,6 +219,5 @@ class SitesPage extends React.PureComponent<SitesPageProps> {
       </QueryComponent>
     );
   }
-}
+} */
 
-export default SitesPage;
