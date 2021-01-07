9e2bad1c9015a8fdd25c108bfc1dd5b02f30b5b6
hifilorau <tomrau@gmail.com>
2021-01-07 15:21:20 -0500

    updated api endpoitn

--- front/src/services/search/api.ts
+++ front/src/services/search/api.ts
@@ -5,7 +5,7 @@ import { callGraphql } from 'utils/graphqlUtil';
 // This is a temporary measure to support different enpoints during the backend migration to NodeJS
 // Once that is complete, all endpoint URLs should be pulled from a common constant
 
-const ENDPOINT = 'http://localhost:3000/graphql'
+const ENDPOINT = `http://${window.location.hostname}:3000/graphql`
 
 export const fetchSearchPageAggs = (searchParams : any) => {
     return callGraphql(ENDPOINT,query.SEARCH_PAGE_AGGS_QUERY, searchParams);
