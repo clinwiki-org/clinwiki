4bb21efd761633cf94b400ad3510e9704a2b6074
chuck <charlesvincentanderson@gmail.com>
2021-01-27 14:39:16 -0500

    pulled in develop

---
+++ front/src/services/study/model/SiteStudyBasicGenericSectionFragment.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: SiteStudyBasicGenericSectionFragment
+// ====================================================
+
+export interface SiteStudyBasicGenericSectionFragment {
+  __typename: "SiteStudyBasicGenericSection";
+  hide: boolean;
+  title: string;
+  name: string;
+}
