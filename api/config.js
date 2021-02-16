c94e8d13b2867add3e0796e2805417eba164e911
jjkahrs <jjkahrs@gmail.com>
2021-02-16 18:06:33 -0500

    Left in actual env data like an idiot

--- api/config.js
+++ api/config.js
@@ -2,16 +2,16 @@ let config = {};
 
 export const loadConfig = () => {
     config.port=process.env.NODE_PORT || 8088;
-    config.postgresUrl= process.env.DATABASE_URL || 'postgres://clinwiki:ZetIFOnXd78H@localhost:15432/clinwiki';
-    config.elasticsearchHost= process.env.ELASTICSEARCH_HOSTS || 'http://localhost:9200';
-    config.elasticsearchUsername= process.env.ELASTICSEARCH_USERNAME || 'elastic';
-    config.elasticsearchPassword= process.env.ELASTICSEARCH_PASSWORD || 'k1o9sbDRgeq2';
-    config.smtpHost= process.env.SMTP_HOST || 'smtp.mailtrap.io';
-    config.smtpPort= process.env.SMTP_PORT || '2525';
-    config.smtpUser= process.env.SMTP_USER || '1e772c663f814c';
-    config.smtpPassword= process.env.SMTP_PASSWORD || 'b84846af96f031';
+    config.postgresUrl= process.env.DATABASE_URL || 'changeme';
+    config.elasticsearchHost= process.env.ELASTICSEARCH_HOSTS || 'changeme';
+    config.elasticsearchUsername= process.env.ELASTICSEARCH_USERNAME || 'changeme';
+    config.elasticsearchPassword= process.env.ELASTICSEARCH_PASSWORD || 'changeme';
+    config.smtpHost= process.env.SMTP_HOST || 'changeme';
+    config.smtpPort= process.env.SMTP_PORT || 'changeme';
+    config.smtpUser= process.env.SMTP_USER || 'changeme';
+    config.smtpPassword= process.env.SMTP_PASSWORD || 'changeme';
     config.outboundEmail= process.env.SMTP_OUTBOUND_EMAIL || 'no-reply@yourdomain.com';
-    config.webUrl= process.env.WEB_URL || 'http://localhost:3001';
+    config.webUrl= process.env.WEB_URL || 'changeme';
     config.jwtSecret = process.env.SECRET_KEY_BASE || 'InsertSecretHere';
     config.jwtExpire = process.env.JWT_EXPIRATION_TIME_SECS || 86400;
     config.googleClientId = process.env.GOOGLE_CLIENT_ID || 'GoogleIdHere'
