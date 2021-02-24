78e94e17c69aef9fd6e7de7bea155faca0bd7753
Rodrigo <45049806+rodctech@users.noreply.github.com>
2021-02-24 13:17:35 -0600

    Update schema.rb
    Back to previous version , active record  version "ActiveRecord::Schema.define(version: 2020_12_02_195235) do"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_220331) do
+ActiveRecord::Schema.define(version: 2020_12_02_195235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,15 +19,6 @@ ActiveRecord::Schema.define(version: 2021_02_18_220331) do
     t.string "label"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "annotations", force: :cascade do |t|
-    t.string "nct_id"
-    t.string "label"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "facet_configs", force: :cascade do |t|
