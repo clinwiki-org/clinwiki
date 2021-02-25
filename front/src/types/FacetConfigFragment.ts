e5ee39822ff04e4efdfde967649a5e331c3d394d
Chuck <charlesvincentanderson@gmail.com>
2021-02-25 12:42:27 -0500

    missing files

---
+++ front/src/types/FacetConfigFragment.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: FacetConfigFragment
+// ====================================================
+
+export interface FacetConfigFragment {
+  __typename: "FacetConfig";
+  id: number;
+  mainConfig: string;
+}
