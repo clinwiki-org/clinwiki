94924b8df1fa60ecba59958e89f0cb4055a0a177
Rod Chavez <rodctech@gmail.com>
2021-03-29 16:47:48 -0500

    rename Hasura enpoiint

--- front/src/services/hasuraSite/api.ts
+++ front/src/services/hasuraSite/api.ts
@@ -1,26 +1,36 @@
-import * as mutate from './mutations'
+import * as mutate from './mutations';
 import * as query from './queries';
-import { callGraphql, callHasura, getHasuraClinwikiURL, callHasuraClinwiki} from 'utils/graphqlUtil';
-                                                                                    
-// This is a temporary measure to support different enpoints during the backend migration to NodeJS
-// Once that is complete, all endpoint URLs should be pulled from a common constant 
+import { getHasuraClinwikiURL, callHasuraClinwiki } from 'utils/graphqlUtil';
 
-const HASURA_ENDPOINT = getHasuraClinwikiURL();                                     
+// This is a temporary measure to support different enpoints during the backend migration to NodeJS
+// Once that is complete, all endpoint URLs should be pulled from a common constant
+
+const HASURA_CW = getHasuraClinwikiURL();
 
 export const updateSiteHasura = (input, url?) => {
-    //console.log('mutation = ', mutate.UPDATE_SITE_MUTATION);
-    //console.log('input = ', input);
-    return callHasuraClinwiki(HASURA_ENDPOINT, mutate.UPDATE_SITE_MUTATION,
-        { id: input.id, name: input.name, subdomain: input.subdomain, hide_donation: input.hide_donation, reactions_config: input.reactions_config, skip_landing: input.skip_landing, themes: input.themes, user_rank: input.user_rank});
-        //{ input: { id: input.id, name: input.name}});
+  //console.log('mutation = ', mutate.UPDATE_SITE_MUTATION);
+  //console.log('input = ', input);
+  return callHasuraClinwiki(HASURA_CW, mutate.UPDATE_SITE_MUTATION, {
+    id: input.id,
+    name: input.name,
+    subdomain: input.subdomain,
+    hide_donation: input.hide_donation,
+    reactions_config: input.reactions_config,
+    skip_landing: input.skip_landing,
+    themes: input.themes,
+    user_rank: input.user_rank,
+  });
+  //{ input: { id: input.id, name: input.name}});
 };
 export const fetchSitesPageHasura = () => {
-    //console.log("fetchSitesPageHasura called in hasuraSite api")
-    return callHasuraClinwiki(HASURA_ENDPOINT, query.SITES_PAGE_QUERY, {});
-}
+  //console.log("fetchSitesPageHasura called in hasuraSite api")
+  return callHasuraClinwiki(HASURA_CW, query.SITES_PAGE_QUERY, {});
+};
 
 export const fetchSiteProviderHasura = (id?, url?) => {
-    //console.log("fetchSiteProviderHasura called in hasuraSite api")
-    return callHasuraClinwiki(HASURA_ENDPOINT, query.SITE_PROVIDER_QUERY,
-        { id: id, url: url});
-}
+  //console.log("fetchSiteProviderHasura called in hasuraSite api")
+  return callHasuraClinwiki(HASURA_CW, query.SITE_PROVIDER_QUERY, {
+    id: id,
+    url: url,
+  });
+};
