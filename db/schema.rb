b1b2296a4465267e8f3b45a3bba70b432fcbe0e8
Rod Chavez <rodctech@gmail.com>
2021-05-24 14:14:08 -0500

    fix for Search Query bug

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,16 @@
 ActiveRecord::Schema.define(version: 2021_03_30_155744) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "annotation_labels", force: :cascade do |t|
+  create_table "annotation_labels", id: :serial, force: :cascade do |t|
     t.string "label"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "annotations", force: :cascade do |t|
+  create_table "annotations", id: :serial, force: :cascade do |t|
     t.string "nct_id"
     t.string "label"
     t.text "description"
@@ -95,10 +96,10 @@ ActiveRecord::Schema.define(version: 2021_03_30_155744) do
     t.index ["user_id"], name: "index_reactions_on_user_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.string "nct_id"
+  create_table "reviews", id: :serial, force: :cascade do |t|
     t.integer "overall_rating"
     t.text "text"
+    t.string "nct_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
@@ -178,8 +179,8 @@ ActiveRecord::Schema.define(version: 2021_03_30_155744) do
     t.text "user_rank", default: "[{\"rank\":\"default\",\"gte\":0},{\"rank\":\"bronze\",\"gte\":26},{\"rank\":\"silver\",\"gte\":51},{\"rank\":\"gold\",\"gte\":75},{\"rank\":\"platinum\",\"gte\":101}] "
     t.text "reactions_config", default: "[]"
     t.boolean "hide_donation"
-    t.text "default_hash"
-    t.text "default_search_page"
+    t.string "default_hash"
+    t.string "default_search_page"
     t.index ["subdomain"], name: "index_sites_on_subdomain", unique: true
   end
 
@@ -191,7 +192,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_155744) do
     t.index ["user_id"], name: "index_study_view_logs_on_user_id"
   end
 
-  create_table "tags", force: :cascade do |t|
+  create_table "tags", id: :serial, force: :cascade do |t|
     t.string "nct_id"
     t.string "value"
     t.datetime "created_at", null: false
@@ -199,7 +200,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_155744) do
     t.integer "user_id"
   end
 
-  create_table "user_session_studies", force: :cascade do |t|
+  create_table "user_session_studies", id: :serial, force: :cascade do |t|
     t.string "nct_id"
     t.text "serialized_study"
     t.datetime "created_at", null: false
@@ -209,7 +210,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_155744) do
     t.index ["user_id"], name: "user_session_studies_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -227,7 +228,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_155744) do
     t.string "picture_url"
     t.string "reset_token_url"
     t.string "search_notification_criteria"
-    t.datetime "search_last_notification", default: "2021-03-10 20:01:44"
+    t.datetime "search_last_notification", default: "2021-03-10 19:42:57"
     t.integer "search_notification_frequency"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -241,9 +242,9 @@ ActiveRecord::Schema.define(version: 2021_03_30_155744) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
-  create_table "wiki_page_edits", force: :cascade do |t|
-    t.bigint "wiki_page_id"
-    t.bigint "user_id"
+  create_table "wiki_page_edits", id: :serial, force: :cascade do |t|
+    t.integer "wiki_page_id"
+    t.integer "user_id"
     t.text "diff"
     t.text "diff_html"
     t.text "comment"
@@ -253,7 +254,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_155744) do
     t.index ["wiki_page_id"], name: "index_wiki_page_edits_on_wiki_page_id"
   end
 
-  create_table "wiki_pages", force: :cascade do |t|
+  create_table "wiki_pages", id: :serial, force: :cascade do |t|
     t.string "nct_id"
     t.text "text"
     t.datetime "created_at", null: false
