9c710b266a359a0a094ef6301f244f19f656400c
hifilorau <tomrau@gmail.com>
2021-05-24 17:10:54 -0400

    fixed typo in name for testing

--- front/src/components/AuthHeader/AuthHeader.tsx
+++ front/src/components/AuthHeader/AuthHeader.tsx
@@ -107,7 +107,7 @@ const AuthHeader = (props) => {
                 target="_blank"
                 eventKey={2}
                 href="https://www.clinwiki.org/make-a-donation/">
-                Donate to ClinWikis
+                Donate to ClinWiki
                     </NavItem>
             }
             <NavItem eventKey={1} href="https://www.clinwiki.org/" target="_blank">
