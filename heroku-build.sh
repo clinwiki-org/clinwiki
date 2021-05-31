d80608cf07c285c1fcdc74e6e9fcff9acc199dad
jjkahrs <jjkahrs@gmail.com>
2021-05-31 18:27:15 -0400

    Some debugging

--- heroku-build.sh
+++ heroku-build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+echo "BUILDING FRONTEND"
 cd front
 yarn install
 yarn run build
 
+echo "BUILDING BACKEND"
 cd ../api
 npm uninstall bcrypt
 npm install bcrypt
 npm install
 npm run build
+
+echo "BUILD COMPLETE"
