6d64d488c3fa108cbe9928d98755be039db021d0
Brian <bespinosa1931@gmail.com>
2021-06-17 16:36:20 -0500

    randNumberLoader prevent whitescreen
    when records total undefined or null would white screen

--- front/src/containers/Islands/RandNumberLoader.tsx
+++ front/src/containers/Islands/RandNumberLoader.tsx
@@ -9,7 +9,7 @@ function RandNumberLoader() {
   const loadingPage = useSelector((state: RootState) => state.study.isFetchingStudy);
   const showLoader = updatingParams || loadingPage
   const studyData = useSelector((state: RootState) => state.study.studyPage);
-  const recordsTotal = studyData?.data.search.recordsTotal;
+  const recordsTotal = studyData?.data?.search?.recordsTotal;
   const [randNumber, setRandNumber] = useState(1234);
   const generateNumber = () => {
     setRandNumber(Math.floor(Math.random() * (100000 - 20 + 1)) + 20);
