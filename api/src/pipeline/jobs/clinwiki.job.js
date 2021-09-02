9b6f3a43423cb5f59262202f28c55b9f70714d48
William Hoos <williamhoos@gmail.com>
2021-09-01 21:30:09 -0400

    reduce chunk size to find failing study

--- api/src/pipeline/jobs/clinwiki.job.js
+++ api/src/pipeline/jobs/clinwiki.job.js
@@ -5,7 +5,7 @@ import {query} from '../../util/db';
 import moment from 'moment';
 const util = require('util')
 
-const CHUNK_SIZE = 1000;
+const CHUNK_SIZE = 1;
 
 export const clinwikiJob = async nctIdList => {
     try {
