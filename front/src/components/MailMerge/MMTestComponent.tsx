2f4b9feef40fe968c9c97119f12a2ff19f2ebdc7
bespinosa01 <bespinosa1931@gmail.com>
2021-09-29 10:51:03 -0500

    update MM test with logic from old MMView

--- front/src/components/MailMerge/MMTestComponent.tsx
+++ front/src/components/MailMerge/MMTestComponent.tsx
@@ -6,7 +6,7 @@ import CollapsiblePanel from 'components/CollapsiblePanel';
 import MailMerge from './MailMerge';
 import { useRouteMatch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { getMyQuery, getSearchNearbyQuery } from 'components/MailMerge/MailMergeUtils';
+import { getMyQuery } from 'components/MailMerge/MailMergeUtils';
 import { studyIslands, searchIslands } from 'containers/Islands/CommonIslands'
 import useUrlParams from 'utils/UrlParamsProvider';
 import { fetchSearchPageMM, fetchStudyPageHasura, fetchStudyPageHasuraDIS, fetchStudyPageNearby } from 'services/study/actions';
@@ -17,7 +17,16 @@ import { useFragment, schemaTokens } from 'components/MailMerge/MailMergeFragmen
 import { introspectionQuery } from 'graphql/utilities';
 import { fetchNodeIntrospection, fetchHasuraIntrospection } from 'services/introspection/actions';
 import { fetchSearchParams } from 'services/search/actions';
-
+import {  useRef } from 'react';
+import  {  applyTemplate, compileTemplate } from 'components/MailMerge/MailMergeView';
+import { convertDisplayName, fetchIslandConfig, fetchSearchPageAggBuckets } from 'services/search/actions'
+import { fetchSuggestedLabels, setShowLoginModal } from '../../services/study/actions';
+import {  getSearchQuery, getHasuraStudyQuery, getSearchQueryDIS, getHasuraStudyQueryDIS, getSearchNearbyQuery } from 'components/MailMerge/MailMergeUtils';
+import { islandTokens } from 'components/MailMerge/MailMergeFragment';
+import { insertPageViewLog } from 'services/genericPage/actions';
+import LoginModal from 'components/LoginModal';
+import { uniq } from 'ramda';
+import useHandlebars from 'hooks/useHandlebars';
 interface Props {
     url?: string;
     arg?: string;
@@ -53,6 +62,12 @@ export default function GenericPageWrapper(props: Props) {
     const suggestedLabels = useSelector((state: RootState) => state.study.suggestedLabels);
     const [template, setTemplate] = useState(currentPage?.template);
 
+    const currentUser = useSelector((state: RootState) => state.user.current);
+    const isFetchingCurrentUser = useSelector((state: RootState) => state.user.isLoading);
+    const islandConfig = useSelector((state: RootState) => state.search.islandConfig);
+    const searchHash = useSelector((state: RootState) => state.search.searchHash);
+    const searchParams = data?.data?.searchParams;
+    const showLoginModal = useSelector((state: RootState) => state.study.showLoginModal);
     const getPageType = (val) => {
         switch (val) {
             case 1:
@@ -180,8 +195,167 @@ export default function GenericPageWrapper(props: Props) {
 
     const title = microMailMerge(currentPage?.title, searchData(pageType)) || "Add a Title";
 
+    useEffect(() => {
+        !isFetchingCurrentUser && dispatch(insertPageViewLog(currentUser?.id || null, window.location.href))
+    }, [dispatch, window.location.href, isFetchingCurrentUser, currentUser]);
+
     const islands = pageType == 'Study' ? studyIslands : searchIslands;
-  
+
+
+    const islandKeys = new Set(Object.keys(islands || {}));
+    const aggIslandsCurrent = useRef({
+        currentAggIslands: [] as any[]
+    })
+    const wfIslandsCurrent = useRef({
+        currentWFIslands: [] as any[]
+    })
+
+    console.log(islandKeys)
+
+
+
+    const isAgg = island => island.name == "agg";
+    const isWFAgg = island => island.name == "wfagg";
+
+    // FETCH ISLAND CONFIG FOR UNIQUE AGG ISLAND IDs AND WF ISLAND IDs
+    useEffect(() => {
+        const islandKeys = islandTokens(currentPage.template)
+
+        aggIslandsCurrent.current.currentAggIslands = islandKeys.filter(isAgg, islandKeys);
+        wfIslandsCurrent.current.currentWFIslands = islandKeys.filter(isWFAgg, islandKeys);
+        let uniqueAggs = uniq(aggIslandsCurrent.current.currentAggIslands);
+        let aggIdArray: any[] = [];
+        let uniqueWFIds = uniq(wfIslandsCurrent.current.currentWFIslands);
+        let wfLabels: any[] = [];
+
+        uniqueWFIds.map((WF) => {
+            wfLabels.push(parseInt(WF.attribs.id))
+        });
+
+        uniqueAggs.map((agg) => {
+            aggIdArray.push(parseInt(agg.attribs.id))
+
+        });
+        !islandConfig && dispatch(fetchIslandConfig(aggIdArray.concat(wfLabels)));
+    }, [dispatch, currentPage]);
+    // END 
+
+    // FETCH BUCKETS FOR ISLANDS 
+    useEffect(() => {
+        // Duplicate code// Refactor // IslandsAggChild is other file
+        let uniqueAggs = uniq(aggIslandsCurrent.current.currentAggIslands);
+        let aggArray: any[] = [];
+        let aggIdArray: any[] = [];
+        let aggBucketsWanted: any[] = [];
+        let aggSortArray: any[] = [];
+        let crowdAggArray: any[] = [];
+        let crowdAggIdArray: any[] = [];
+        let crowdBucketsWanted: any[] = [];
+        let crowdAggSortArray: any[] = [];
+
+        islandConfig && uniqueAggs.map((agg) => {
+            const aggID = agg.attribs.id;
+            if (islandConfig[aggID]?.defaultToOpen == true) {
+                let sort = {
+                    id: islandConfig[aggID].order.sortKind,
+                    desc: islandConfig[aggID].order.desc
+                };
+
+                islandConfig[aggID].aggKind == 'crowdAggs' && crowdAggArray.push(islandConfig[aggID].name);
+                islandConfig[aggID].aggKind == 'crowdAggs' && crowdAggIdArray.push({ id: aggID, name: islandConfig[aggID].name });
+                islandConfig[aggID].aggKind == 'crowdAggs' && crowdBucketsWanted.push(islandConfig[aggID].visibleOptions);
+                islandConfig[aggID].aggKind == 'crowdAggs' && crowdAggSortArray.push(sort);
+                islandConfig[aggID].aggKind == 'aggs' && aggArray.push(islandConfig[aggID].name);
+                islandConfig[aggID].aggKind == 'aggs' && aggIdArray.push({ id: aggID, name: islandConfig[aggID].name });
+                islandConfig[aggID].aggKind == 'aggs' && aggBucketsWanted.push(islandConfig[aggID].visibleOptions);
+                islandConfig[aggID].aggKind == 'aggs' && aggSortArray.push(sort);
+            }
+        });
+
+
+        if (searchParams && crowdAggArray.length !== 0 || searchParams && aggArray.length !== 0) {
+
+            crowdAggArray.forEach((agg, i) => {
+
+                const variables = {
+                    ...searchParams.searchParams,
+                    url: params.sv,
+                    configType: 'presearch',
+                    returnAll: false,
+                    agg: `fm_${agg}`,
+                    aggOptionsSort: crowdAggSortArray[i],
+                    pageSize: 100,
+                    page: 1,
+                    q: searchParams.searchParams.q,
+                    aggBucketsWanted: crowdBucketsWanted[i]
+
+                };
+
+                dispatch(fetchSearchPageAggBuckets(variables, crowdAggIdArray[i].id))
+            });
+            aggArray.forEach((agg, i) => {
+
+                const variables = {
+                    ...searchParams.searchParams,
+                    url: params.sv,
+                    configType: 'presearch',
+                    returnAll: false,
+                    agg: agg,
+                    aggOptionsSort: aggSortArray[i],
+                    pageSize: 25,
+                    page: 1,
+                    q: searchParams.searchParams.q,
+                    aggBucketsWanted: aggBucketsWanted[i]
+
+                };
+
+                dispatch(fetchSearchPageAggBuckets(variables, aggIdArray[i].id))
+            });
+        }
+
+    }, [dispatch, islandConfig, searchHash, searchParams])
+    // END 
+
+    // FETCH BUCKETS FOR WF 
+    useEffect(() => {
+        let uniqueWFIds = uniq(wfIslandsCurrent.current.currentWFIslands);
+        let wfLabels: any[] = [];
+
+        islandConfig && uniqueWFIds.map((WF) => {
+            const wfID = WF.attribs.id;
+            if (islandConfig[wfID]?.defaultToOpen == true) {
+                islandConfig[wfID] && wfLabels.push(islandConfig[wfID].name);
+            }
+        })
+
+        if (wfLabels) {
+            //@ts-ignore
+            wfLabels[0] && dispatch(fetchSuggestedLabels(searchData(pageType)?.nct_id, wfLabels));
+
+        }
+
+
+    }, [dispatch, islandConfig])
+    // END
+    // WFISLANDS CONVERTDISPLAY NAME 
+
+    useEffect(() => {
+        let uniqueWFIds = uniq(wfIslandsCurrent.current.currentWFIslands);
+
+        suggestedLabels && uniqueWFIds.map((WF) => {
+            const wfID = WF.attribs.id;
+            let currentKeyObjects = suggestedLabels.data.crowd_keys.filter((x) => x.crowd_key === islandConfig[wfID].name)
+
+            if (islandConfig[wfID]?.defaultToOpen == true) {
+                const compiled = islandConfig[wfID] && compileTemplate(islandConfig[wfID].displayName)
+                const raw = applyTemplate(compiled, currentKeyObjects[0])
+                if (raw !== islandConfig[wfID].displayName) {
+                    dispatch(convertDisplayName(raw, wfID))
+                }
+            }
+        })
+
+    }, [suggestedLabels])  
 
 
   
