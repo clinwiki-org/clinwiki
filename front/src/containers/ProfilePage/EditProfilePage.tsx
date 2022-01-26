8bb5619cfa6d3a717f5d513565276c0bab14b3ce
bespinosa01 <bespinosa1931@gmail.com>
2022-01-26 16:10:49 -0600

    profile page face-lift

--- front/src/containers/ProfilePage/EditProfilePage.tsx
+++ front/src/containers/ProfilePage/EditProfilePage.tsx
@@ -24,6 +24,8 @@ import UserSavedSearches from './components/UserSavedSearches';
 import UserSavedDocuments from './components/UserSavedDocuments';
 import { RootState } from 'reducers';
 import { fetchReactionsById } from './../../services/study/actions'
+import { sentanceCaseFromCamelCase } from 'utils/helpers';
+
 interface EditProfilePageProps {
   //@ts-ignore
   user: UserFragment | null;
@@ -31,60 +33,141 @@ interface EditProfilePageProps {
   location: Location;
   match: match;
 }
+let editableFields = ['firstName', 'lastName', 'email', 'defaultQueryString'];
 
 const ProfileInfo = ({ user, onToggle }) => {
-  let pictureUrl = user.pictureUrl || null;
+  // let pictureUrl = user.pictureUrl || null; 
+  const renderFields = () =>
+    editableFields.map((field) => {
+
+      let userValue = user[field];
+      if (userValue) {
+
+
+        return (
+
+          <li className="flex flex-row ">
+            <div className="select-none flex flex-1 items-center p-4">
+              <div className="flex-1 pl-1 mr-16">
+
+                <div className="font-medium dark:text-white">
+                  {/* {user?.firstName  ?? 'None'} */}
+                  {userValue}
+                </div>
+                <div className="text-gray-600 dark:text-gray-200 text-sm">
+                  {sentanceCaseFromCamelCase(field)}
+                </div>
+              </div>
+            </div>
+          </li>
+        )
+      }
+    })
   return (
-    <div>
-      <ProfilePicture pictureUrl={pictureUrl} />
-      <a
-        style={{ paddingLeft: '15px', cursor: 'pointer' }}
-        onClick={onToggle}>
-        {' '}
-      Edit Profile
-    </a>
+    <>
+      <div className="container flex flex-col mx-auto w-full items-center justify-center bg-white dark:bg-gray-800 rounded-lg shadow">
+        <div className="px-4 py-5 sm:px-6 border-b w-full">
+          <h3 className="font-bold text-2xl leading-6 font-medium text-gray-900 dark:text-white">
+            My Profile
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            <a className='cursor-pointer'
+              onClick={onToggle}>
+              {' '}
+              Edit Profile
+            </a>
+            {/* By saving a study you are subscribing to weekly updates and email notifications.  */}
+          </p>
+          <div className="flex items-center bg-blue-400 text-white text-sm font-bold px-4 py-3" role="alert">
+            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" /></svg>
+            <p>Please update your profile. The following fields are empty: {
+              editableFields.map((field, i) => {
 
-      <ThemedSearchContainer>
-        <StyledProfileLabel>First Name:</StyledProfileLabel>
-        <StyledProfileValue>
-          {' '}
-          {user.firstName}
-        </StyledProfileValue>
-        <StyledProfileLabel>Last Name: </StyledProfileLabel>
-        <StyledProfileValue>{user.lastName}</StyledProfileValue>
-        <StyledProfileLabel>E-mail: </StyledProfileLabel>
-        <StyledProfileValue>{user.email}</StyledProfileValue>
-        <StyledProfileLabel>Default Query String: </StyledProfileLabel>
-        <StyledProfileValue>
-          {user.defaultQueryString || 'N/A'}
-        </StyledProfileValue>
-      </ThemedSearchContainer>
-    </div>
+                let userValue = user[field];
+                return !userValue && `${sentanceCaseFromCamelCase(field)} ${i + 1 < editableFields.length ? (',') : ('')}`
+              })
 
+            }</p>
+          </div>
+        </div>
+
+        <ul className="flex flex-col divide divide-y w-full">
+          {renderFields()}
+        </ul>
+      </div>
+    </>
   );
-}
 
+}
 const EditForm = ({ user, onToggle }) => {
   const dispatch = useDispatch();
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [defaultQueryString, setDefaultQueryString] = useState('');
-
   const handleEditProfile = () => {
     dispatch(editProfile(firstName, lastName, defaultQueryString));
     onToggle();
   };
-
+  const handleFormInput = (field, input) => {
+    console.log(field, input)
+    // 'firstName', 'lastName', 'email','defaultQueryString'
+    switch (field) {
+      case 'firstName':
+        setFirstName(input);
+        return
+      case 'lastName':
+        setLastName(input);
+        return
+      case 'email':
+        return
+      case 'defaultQueryString':
+        setDefaultQueryString(input);
+        return
+      default:
+        return
+    }
+  }
   return (
-    <div>
-      <ProfilePicture pictureUrl={user.pictureUrl} />
-      <a
-        style={{ paddingLeft: '15px', cursor: 'pointer' }}
-        onClick={onToggle}>
-        X
-    </a>
-      <ThemedSearchContainer>
-        <StyledProfileLabel>First Name:</StyledProfileLabel>
+    <div className="container flex flex-col mx-auto w-full items-center justify-center bg-white dark:bg-gray-800 rounded-lg shadow">
+      <div className="px-4 py-5 sm:px-6 border-b w-full">
+        <h3 className="font-bold text-2xl leading-6 font-medium text-gray-900 dark:text-white">
+          My Profile
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+          <a className='cursor-pointer'
+            onClick={onToggle}>
+            {' '}
+            X
+          </a>
+        </p>
+        <div>
+
+          {editableFields.map((field) => {
+            let userValue = user[field];
+
+            return (
+
+              <li className="flex flex-row ">
+                <div className="select-none flex flex-1 items-center p-4">
+                  <div className="flex-1 pl-1 mr-16">
+                    <div className="font-medium dark:text-white">
+                      <input onChange={(ev) => handleFormInput(field, ev.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id={field} type="text" value={userValue} />
+                    </div>
+                    <div className="text-gray-600 dark:text-gray-200 text-sm">
+                      {sentanceCaseFromCamelCase(field)}
+                    </div>
+                  </div>
+                </div>
+              </li>
+            )
+
+          })}
+
+          <ThemedButton onClick={handleEditProfile}>
+            Save
+          </ThemedButton>
+          {/* <ThemedSearchContainer> */}
+          {/* <StyledProfileLabel>First Name:</StyledProfileLabel>
         <StyledProfileForm
           name="firstName"
           placeholder="First name"
@@ -92,7 +175,6 @@ const EditForm = ({ user, onToggle }) => {
           onChange={(ev) => setFirstName(ev.target.value)}
         />
         <StyledProfileLabel>Last Name:</StyledProfileLabel>
-
         <StyledProfileForm
           name="lastName"
           placeholder="Last name"
@@ -100,7 +182,6 @@ const EditForm = ({ user, onToggle }) => {
           onChange={(ev) => setLastName(ev.target.value)}
         />
         <StyledProfileLabel>Default Query String:</StyledProfileLabel>
-
         <StyledProfileForm
           name="defaultQueryString"
           placeholder="Default query string"
@@ -109,42 +190,37 @@ const EditForm = ({ user, onToggle }) => {
         />
         <ThemedButton onClick={handleEditProfile}>
           Save
-      </ThemedButton>
-      </ThemedSearchContainer>
+      </ThemedButton> */}
+          {/* </ThemedSearchContainer> */}
+        </div>
+      </div>
     </div>
   );
 }
-
 const EditProfilePage = (props: EditProfilePageProps) => {
   const [isEditing, setEditing] = useState(false);
   const [currentDisplay, setCurrentDisplay] = useState('contributions');
   const [totalContributions, setTotalContributions] = useState('');
   const user = useSelector((state: RootState) => state.user.current);
   const reactions = useSelector((state: RootState) => state.study.reactionsById);
-
   const dispatch = useDispatch();
-
   const ids = ["1", "2", "3", "4"]
   useEffect(() => {
     ids.map(id => {
       dispatch(fetchReactionsById(id))
     });
   }, [dispatch]);
-
   if (!user) // || !user.reactions)  //! Taking or out to avoid No user error on profile page load, not sure why this check was in.
   {
     return <div>No user</div>;
   }
-
   let userContributions = user?.contributions;
   let reviewCount = user?.reviewCount;
   let rank = user?.rank;
   let totalcount = 0;
-
   user.reactionsCount?.map(reaction => {
     totalcount += reaction.count;
   });
-
   const handleTotalContributions = recordsTotal => {
     if (recordsTotal !== totalContributions) {
       setTotalContributions(recordsTotal);
@@ -152,8 +228,6 @@ const EditProfilePage = (props: EditProfilePageProps) => {
     }
     return;
   };
-
-
   const renderResults = user => {
     const email = user.email;
     switch (currentDisplay) {
@@ -187,7 +261,6 @@ const EditProfilePage = (props: EditProfilePageProps) => {
         return (
           <div>
             {/* <h2>Liked Studies:</h2> */}
-
             {idArray.map(id => {
               const displaySub = ID => {
                 switch (ID) {
@@ -201,10 +274,7 @@ const EditProfilePage = (props: EditProfilePageProps) => {
                     return 'Skull and Cross Studies';
                 }
               };
-
               return (
-
-
                 reactions ? (
                   <span>
                     <h2>
@@ -224,30 +294,36 @@ const EditProfilePage = (props: EditProfilePageProps) => {
         );
     }
   };
-
   return (
     <ThemedMainContainer>
-      <h2>My profile</h2>
-      {isEditing === true
-        ? <EditForm user={user} onToggle={() => setEditing(false)} />
-        : <ProfileInfo user={user} onToggle={() => setEditing(true)} />}
 
-      <h2>My Searches</h2>
-      <ThemedSearchContainer>
-        <h4>Saved Searches:</h4>
-        <UserSavedSearches
-          user={user}
-        />
-      </ThemedSearchContainer>
-      <h2>My Saved Studies</h2>
-      <ThemedSearchContainer>
-        <h4>Studies:</h4>
-        <UserSavedDocuments
-          user={user}
-        />
-      </ThemedSearchContainer>
+      {/* <span className='px-20 font-bold text-2xl'>My Saved Searches</span> */}
+      {/* <ThemedSearchContainer> */}
+      {/* <ProfilePicture pictureUrl={user.pictureUrl} /> */}
 
-        {/* need to read add all of these */}
+      <div className="container flex flex-col mx-auto w-full items-center justify-center">
+        <ul className="flex flex-col">
+
+          <li className="border-gray-400 flex flex-row mb-16">
+            {isEditing === true
+              ? <EditForm user={user} onToggle={() => setEditing(false)} />
+              : <ProfileInfo user={user} onToggle={() => setEditing(true)} />}
+          </li>
+          <li className="border-gray-400 flex flex-row mb-16">
+
+            <UserSavedSearches
+              user={user}
+            />
+          </li>
+          <li className="border-gray-400 flex flex-row mb-16">
+
+            <UserSavedDocuments
+              user={user}
+            />
+          </li>
+        </ul>
+      </div>
+      {/* need to read add all of these */}
       {/* <h2>My Contributions</h2>
       <ThemedSearchContainer>
         <ProfileScoreBoard
@@ -268,8 +344,6 @@ const EditProfilePage = (props: EditProfilePageProps) => {
         <div>No User</div>
       )}
     </ThemedMainContainer>
-
   );
 };
-
 export default EditProfilePage;
--- front/src/containers/ProfilePage/components/UserSavedDocuments.tsx
+++ front/src/containers/ProfilePage/components/UserSavedDocuments.tsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { RootState } from 'reducers';
 import { BeatLoader } from 'react-spinners';
+import * as FontAwesome from 'react-fontawesome';
 
 interface userSavedDocsProps {
   //@ts-ignore
@@ -22,7 +23,7 @@ interface userSavedDocsState {
 
 export default function UserSavedDocs(props: userSavedDocsProps) {
   const userId = props.user!.id;
-  
+
   const userSavedDocs = useSelector((state: RootState) => state.search.savedDocs);
   const dispatch = useDispatch();
 
@@ -44,30 +45,55 @@ export default function UserSavedDocs(props: userSavedDocsProps) {
     return <BeatLoader />
   }
 
-  const savedDocs = userSavedDocs.data.saved_documents; 
+  const savedDocs = userSavedDocs.data.saved_documents;
   // console.log('saved seraches', savedDocs)
   return (
+
     (savedDocs && savedDocs?.length !== 0) ?
-      <>{
-        savedDocs.map((list) => (
-          <StyledProfileLogValue key={list?.id}>
-            <a href={list.url}> {list?.name_label} </a>: {list?.document_id}
-            <div style={{ float: 'right', margin: "1px 2px" }} >
-              <LabeledButton
-                helperText={"Delete Study"}
-                theClick={handleDeleteSavedList(list?.id)}
-                iconName={"trash"}
-              />
-              <a href={list?.url}>
-              <LabeledButton
-                helperText={"Navigate to Link"}
-                iconName={"link"}
-              />
-              </a>
-            </div>
-          </StyledProfileLogValue>
-        ))
-      }
+      <>
+        <div className="container flex flex-col mx-auto w-full items-center justify-center bg-white dark:bg-gray-800 rounded-lg shadow">
+          <div className="px-4 py-5 sm:px-6 border-b w-full">
+            <h3 className="font-bold text-2xl leading-6 font-medium text-gray-900 dark:text-white">
+              My Saved Studies
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              By saving a study you are subscribing to weekly updates and email notifications.
+            </p>
+          </div>
+          <ul className="flex flex-col divide divide-y w-full">
+
+            {
+              savedDocs.map((list) => (
+                <li className="flex flex-row ">
+                  <div className="select-none flex flex-1 items-center p-4">
+                    <div className="flex-1 pl-1 mr-16">
+                      <a href={list?.url}>
+                        <div className="font-medium dark:text-white">
+                          {list?.name_label}
+                        </div>
+                        <div className="text-gray-600 dark:text-gray-200 text-sm">
+                          {list?.document_id}
+                        </div>
+                      </a>
+                    </div>
+                    <div className="cursor-pointer">
+                      <FontAwesome onClick={handleDeleteSavedList(list.id)} className="hover:text-red-500 dark:hover:text-white dark:text-gray-200 text-gray-500" name={'trash'} />
+                    </div>
+                    <a href={list?.url}>
+                      <button className="w-24 text-right flex justify-end">
+                        <svg width="20" fill="currentColor" height="20" className="hover:text-gray-800 dark:hover:text-white dark:text-gray-200 text-gray-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
+                          </path>
+                        </svg>
+                      </button>
+                    </a>
+                  </div>
+                </li>
+
+              ))
+            }
+          </ul>
+        </div>
       </>
       :
       <StyledProfileLabel>
